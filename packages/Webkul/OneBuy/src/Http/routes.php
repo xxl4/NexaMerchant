1e272dc145dabd00cbb65eb14ae5a6bdaafe8ed8
Steve <nice.lizhi@gmail.com>
2023-12-15 07:43:43 +0000

    add onebuy

--- packages/Webkul/OneBuy/src/Http/routes.php
+++ packages/Webkul/OneBuy/src/Http/routes.php
@@ -3,11 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use Nicelizhi\OneBuy\Http\Controllers\ProductController;
 
-Route::group(['middleware' => ['locale', 'theme', 'currency']], function () {
+Route::group(['middleware' => ['locale', 'theme', 'currency','web']], function () {
 
     Route::get('onebuy/{slug}', [ProductController::class, 'detail'])
         ->name('onebuy.product.page')
         ->middleware('cacheResponse');
+    
+    Route::any("onebuy/order/add/sync", [ProductController::class, "order_add_sync"])
+        ->name("onebuy.order.add.sync");
+
+
 
 });
 
