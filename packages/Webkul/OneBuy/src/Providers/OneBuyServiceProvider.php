b71f1d0d03126f1d7212b0ef6b00a673cdacb978
Steve <nice.lizhi@gmail.com>
2023-12-15 07:43:57 +0000

    add onebuy

--- packages/Webkul/OneBuy/src/Providers/OneBuyServiceProvider.php
+++ packages/Webkul/OneBuy/src/Providers/OneBuyServiceProvider.php
@@ -2,7 +2,15 @@
 
 namespace Nicelizhi\OneBuy\Providers;
 
+use Illuminate\Pagination\Paginator;
+use Illuminate\Routing\Router;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
+use Webkul\Shop\Http\Middleware\AuthenticateCustomer;
+use Webkul\Shop\Http\Middleware\Currency;
+use Webkul\Shop\Http\Middleware\Locale;
+use Webkul\Shop\Http\Middleware\Theme;
 
 class OneBuyServiceProvider extends ServiceProvider
 {
@@ -11,12 +19,28 @@ class OneBuyServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(Router $router)
     {
         
         include __DIR__ . '/../Http/routes.php';
 
         $this->loadViewsFrom(__DIR__ . '/../Resources/views', 'onebuy');
+
+        /* aliases */
+        $router->aliasMiddleware('currency', Currency::class);
+        $router->aliasMiddleware('locale', Locale::class);
+        $router->aliasMiddleware('customer', AuthenticateCustomer::class);
+        $router->aliasMiddleware('theme', Theme::class);
+
+        /* View Composers */
+        //$this->composeView();
+
+        /* Paginator */
+        Paginator::defaultView('shop::partials.pagination');
+        Paginator::defaultSimpleView('shop::partials.pagination');
+
+        Blade::anonymousComponentPath(__DIR__ . '/../Resources/views/components', 'onebuy');
+
         /*
         $this->app->register(EventServiceProvider::class);
         */
