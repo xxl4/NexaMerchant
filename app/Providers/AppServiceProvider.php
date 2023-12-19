7b1de2f40eaaa90fe72279a0042f0a1cc1cfe59f
Steve <nice.lizhi@gmail.com>
2023-12-19 02:05:40 +0000

    upgrade to https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Schema::defaultStringLength(191);
+        \Illuminate\Support\Facades\URL::forceScheme('https');
     }
 
     /**
