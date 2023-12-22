66f3bb64f12fd1bb2ac16df767e3d8e7952f7d71
Steve <nice.lizhi@gmail.com>
2023-12-22 05:14:16 +0000

    add shopify order

---
+++ packages/Webkul/Shopify/src/Console/Commands/Order/Get.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Nicelizhi\Shopify\Console\Commands\Order;
+
+use Illuminate\Console\Command;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Log;
+
+class Get extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'shopify:order:get';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Get Order List';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $client = new Client();
+
+        $shopify = config("shopify");
+
+
+        /**
+         * 
+         * @link https://shopify.dev/docs/api/admin-rest/2023-10/resources/order#get-orders?status=any
+         * 
+         */
+        $response = $client->get($shopify['shopify_app_host_name'].'/admin/api/2023-10/orders.json?status=any', [
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'Accept' => 'application/json',
+                'X-Shopify-Access-Token' => $shopify['shopify_admin_access_token'],
+            ]
+        ]);
+
+        $body = json_decode($response->getBody(), true);
+
+        $body = $response->getBody();
+        //Log::info($body);
+        $body = json_decode($body, true);
+        foreach($body['orders'] as $key=>$item) {
+
+            Log::info(json_encode($item));
+           var_dump($item);exit;
+
+        }
+    }
+}
