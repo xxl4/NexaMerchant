9de845c972a477d22173b87518d9c6d2d29a4d21
Steve <nice.lizhi@gmail.com>
2023-12-25 23:17:02 +0000

    debug the airwallex create payment

--- packages/Webkul/Airwallex/src/Sdk/Airwallex.php
+++ packages/Webkul/Airwallex/src/Sdk/Airwallex.php
@@ -4,6 +4,9 @@ namespace Nicelizhi\Airwallex\Sdk;
 
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Request;
+use Illuminate\Support\Facades\Config;
+use GuzzleHttp\Promise;
+use Illuminate\Support\Facades\Cache;
 
 class Airwallex {
 
@@ -31,7 +34,7 @@ class Airwallex {
      * 
      * 
      */
-    public function __construct($config, $productionMode) {
+    public function __construct($config=array(), $productionMode) {
         $this->clientEmail = $config['clientEmail'];
         $this->clientPassword = $config['clientPassword'];
         $this->clientId = $config['clientId'];
@@ -39,6 +42,8 @@ class Airwallex {
         $this->paDC = $config['paDC'];
         $this->accountDC = $config['accountDC'];
         $this->apiKey = $config['apiKey'];
+
+        if($productionMode==1) $this->host = "https://api.airwallex.com"; 
     }
 
     /**
@@ -48,14 +53,48 @@ class Airwallex {
      * 
      */
     public function Authentication() {
-        $headers = [
-            'x-client-id' => $this->clientId,
-            'x-api-key'   => $this->apiKey
-        ];
-        $body = [];
-        $request = new Request('POST', $this->host."/api/v1/authentication/login", $headers, $body);
-        $promise = $client->sendAsync($request);
-        return $promise;
+
+
+        $cache_key = "airwallex_token";
+
+        $token = Cache::get($cache_key);
+
+        //var_dump($token);
+
+        if(!Cache::has($cache_key)) {
+            $client = new Client([
+                // Base URI is used with relative requests
+                'base_uri' => $this->host,
+                // You can set any number of default request options.
+                'timeout'  => 2.0,
+            ]);
+    
+            $headers = [
+                'x-client-id' => $this->clientId,
+                'x-api-key'   => $this->apiKey
+            ];
+            //$body = "";
+            //$request = new Request('POST', $this->host."/api/v1/authentication/login", $headers, $body);
+            //$promise = $client->sendAsync($request);
+            //$responses = Promise\Utils::unwrap($promise);
+    
+    
+            $response = $client->request('POST', "/api/v1/authentication/login", [ 
+                'headers' => [
+                     'Accept' => 'application/json', 
+                     'x-client-id' => $this->clientId, 
+                     'x-api-key' => $this->apiKey, 
+                ]
+            ]);
+    
+            $body = $response->getBody();
+            var_dump($body);
+            $json = json_decode($body);
+            Cache::put($cache_key, $json->token, 3600);
+            return $json->token;
+        }else{
+            return $token;
+        }
     }
 
     /**
@@ -64,8 +103,32 @@ class Airwallex {
      * 
      * 
      */
-    public function CreatePayment() {
+    public function CreatePayment($data) {
+        $token = $this->Authentication();
 
+        $client = new Client([
+            // Base URI is used with relative requests
+            'base_uri' => $this->host,
+            // You can set any number of default request options.
+            'timeout'  => 2.0,
+        ]);
+
+        //var_dump(json_decode($data));
+
+        //var_dump($token, $data);
+
+        var_dump(json_decode($data));
+
+        $response = $client->request('POST', "/api/v1/pa/payment_intents/create", [ 
+            'headers' => [
+                 'Accept' => 'application/json', 
+                 'Authorization' => "Bearer ".$token, 
+            ],
+            'json' => $data,
+            'debug' => true
+        ]);
+        $body = $response->getBody();
+        var_dump($body, $data, $token);
     }
 
     public function CheckPaymentStatus() {
@@ -87,6 +150,34 @@ class Airwallex {
      */
     public function GetQuote() {
 
+    }
+
+    public function getPaymentMethods() {
+        $paymentMethods = [];
+
+        foreach (Config::get('payment_methods') as $paymentMethod) {
+            
+            $object = app($paymentMethod['class']);
+
+            if ($object->isAvailable()) {
+                $paymentMethods[] = [
+                    'method'       => $object->getCode(),
+                    'method_title' => $object->getTitle(),
+                    'description'  => $object->getDescription(),
+                    'sort'         => $object->getSortOrder(),
+                ];
+            }
+        }
+
+        usort ($paymentMethods, function($a, $b) {
+            if ($a['sort'] == $b['sort']) {
+                return 0;
+            }
+
+            return ($a['sort'] < $b['sort']) ? -1 : 1;
+        });
+
+        return $paymentMethods;
     }
 
 }
