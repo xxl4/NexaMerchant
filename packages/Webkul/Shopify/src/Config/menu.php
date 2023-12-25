6f10c02ccdd9da3985bdadea3ff82d48506b15f9
Steve <nice.lizhi@gmail.com>
2023-12-25 01:05:07 +0000

    add menu and acl config

---
+++ packages/Webkul/Shopify/src/Config/menu.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    /**
+     * Dashboard.
+     */
+    [
+        'key'        => 'shopify',
+        'name'       => 'admin::shopify.products.index',
+        'route'      => 'shopify.products.index',
+        'sort'       => 10,
+        'icon'       => 'icon-dashboard',
+    ]
+];
