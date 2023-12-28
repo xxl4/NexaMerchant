da0b2bd2e1b561923381c1df189a6663a4a3f440
Steve <nice.lizhi@gmail.com>
2023-12-28 07:14:14 +0000

    add shopify order model

---
+++ packages/Webkul/Shopify/src/Models/ShopifyOrder.php
@@ -0,0 +1,31 @@
+<?php
+namespace Nicelizhi\Shopify\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Webkul\Core\Eloquent\TranslatableModel;
+use Webkul\Core\Models\ChannelProxy;
+use Konekt\Concord\Proxies\ModelProxy;
+use Illuminate\Database\Eloquent\Model;
+use Nicelizhi\Shopify\Contracts\ShopifyOrder as ShopifyOrderContract;
+
+
+class ShopifyOrder extends Model implements ShopifyOrderContract
+{
+    use HasFactory;
+
+    /**
+     * Castable.
+     *
+     * @var array
+     */
+
+    /**
+     * Add fillable properties
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'order_id',
+        'shopify_order_id',
+    ];
+}
