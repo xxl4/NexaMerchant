efa384a60f13c09db36d710dd743d325f7484785
Steve <nice.lizhi@gmail.com>
2023-12-28 01:43:25 +0000

    add airwallex lang

---
+++ packages/Webkul/Airwallex/src/Resources/lang/en/app.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'system' => [
+        'title' => 'Airwallex',
+        'info' => 'Airwallex: Trusted Global Payments &amp; Financial Platform.',
+        'api-key' => 'API Key',
+        'production' => 'Production mode',
+        'order-id-prefix' => 'Order ID Prefix',
+        'order-id-prefix' => 'Order ID Prefix',
+        'clientEmail' => 'clientEmail',
+        'clientPassword' => 'clientPassword',
+        'accountId' => 'accountId',
+        'clientId' => 'clientId',
+        'apiKey' => 'apiKey',
+        'paDC' => 'paDC',
+        'accountDC' => 'accountDC',
+
+        'order-id-prefix-info' => 'This is optinal when you have multiple channels'
+    ]
+];
