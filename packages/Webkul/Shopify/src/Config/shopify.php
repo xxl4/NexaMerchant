39b1a4486583590b47f59c0a41648953ee54bb4d
Steve <nice.lizhi@gmail.com>
2024-01-12 09:35:24 +0000

    delete shopify

--- packages/Webkul/Shopify/src/Config/shopify.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'shopify_app_host_name' => env('SHOPIFY_APP_HOST_NAME', "https://wmshoe.myshopify.com"),
-    'shopify_admin_access_token'  => env('SHOPIFY_ADMIN_ACCESS_TOKEN'),
-    'shopify_client_id'  => env('SHOPIFY_CLIENT_ID'),
-    'shopify_client_secret'  => env('SHOPIFY_CLIENT_SECRET'),
-    'shopify_store_id' => env('SHOPIFY_APP_ID')
-];
--- packages/Webkul/Shopify/tests/ShopifyTestCase.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Nicelizhi\Shopify\Tests;
-
-use Tests\TestCase;
-
-class ShopifyTestCase extends TestCase
-{
-}
