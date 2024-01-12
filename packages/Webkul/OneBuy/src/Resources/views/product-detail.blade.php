f1314f3cd47ed355d61d38fe9b4aeb3ed655713b
Steve <nice.lizhi@gmail.com>
2024-01-12 06:48:05 +0000

    fix the size style

--- packages/Webkul/OneBuy/src/Resources/views/product-detail.blade.php
+++ packages/Webkul/OneBuy/src/Resources/views/product-detail.blade.php
@@ -692,7 +692,7 @@ Hatmeo offers a 30 day guarantee on all unused purchases. Simply send the item(s
 </div>
 </div>
 </div>
-<div class="submit-block">
+<div class="submit-block" style="padding-bottom: 10px;">
     <div class="submit-content">
         <div id="checkout-error" style="color:#e51f28;display:none;"></div>
         <div class="pay-width-paypal-standard">
@@ -701,7 +701,7 @@ Hatmeo offers a 30 day guarantee on all unused purchases. Simply send the item(s
         <!-- <button class="submit-button" onclick="checkout()">Pay With Paypal Standard </button> -->
     </div>
 </div>
-<div class="submit-block" style="padding-bottom: 5px;">
+<div class="submit-block">
     <div class="submit-content">
         <div id="checkout-error" style="color:#e51f28;display:none;"></div>
         <button class="submit-button" onclick="checkout()">Pay With credit card</button>
