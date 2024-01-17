c7c40fd9275c6460b8595d73144afef29d7d1bc1
Steve <nice.lizhi@gmail.com>
2024-01-17 04:04:35 +0000

    fix tag

--- packages/Webkul/Tax/src/Models/TaxCategory.php
+++ packages/Webkul/Tax/src/Models/TaxCategory.php
@@ -3,11 +3,19 @@
 namespace Webkul\Tax\Models;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+<<<<<<< HEAD
 use Illuminate\Database\Eloquent\Model;
 use Webkul\Tax\Database\Factories\TaxCategoryFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Webkul\Tax\Contracts\TaxCategory as TaxCategoryContract;
+=======
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Webkul\Tax\Contracts\TaxCategory as TaxCategoryContract;
+use Webkul\Tax\Database\Factories\TaxCategoryFactory;
+>>>>>>> 6db7346497c8511a570d5e8471c9287634998b61
 
 class TaxCategory extends Model implements TaxCategoryContract
 {
@@ -18,7 +26,10 @@ class TaxCategory extends Model implements TaxCategoryContract
      *
      * @var array
      */
+<<<<<<< HEAD
 
+=======
+>>>>>>> 6db7346497c8511a570d5e8471c9287634998b61
     protected $table = 'tax_categories';
 
     protected $fillable = [
@@ -36,11 +47,18 @@ class TaxCategory extends Model implements TaxCategoryContract
 
     /**
      * Create a new factory instance for the model.
+<<<<<<< HEAD
      *
      * @return Factory
+=======
+>>>>>>> 6db7346497c8511a570d5e8471c9287634998b61
      */
     protected static function newFactory(): Factory
     {
         return TaxCategoryFactory::new();
     }
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> 6db7346497c8511a570d5e8471c9287634998b61
