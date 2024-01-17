c0c1f638e67942a43973e40abb6f3e3d850a0506
Steve <nice.lizhi@gmail.com>
2024-01-17 03:16:34 +0000

    fix tests

--- tests/Pest.php
+++ tests/Pest.php
@@ -12,11 +12,13 @@
 */
 
 uses(Webkul\Admin\Tests\AdminTestCase::class)->in('../packages/Webkul/Admin/tests');
+uses(Webkul\Core\Tests\CoreTestCase::class)->in('../packages/Webkul/Core/tests');
 uses(Webkul\DataGrid\Tests\DataGridTestCase::class)->in('../packages/Webkul/DataGrid/tests');
 uses(Webkul\Shop\Tests\ShopTestCase::class)->in('../packages/Webkul/Shop/tests');
 
 uses(Nicelizhi\Shopify\Tests\ShopifyTestCase::class)->in('../packages/Nicelizhi/Shopify/tests');
 uses(Nicelizhi\Checkout\Tests\CheckoutTestCase::class)->in('../packages/Nicelizhi/Checkout/tests');
+
 
 /*
 |--------------------------------------------------------------------------
