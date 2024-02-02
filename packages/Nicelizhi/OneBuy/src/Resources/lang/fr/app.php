0df91c01dc913cd7b06f98b4e3364326435fb92f
Steve <nice.lizhi@gmail.com>
2024-02-02 05:48:48 +0000

    fix fr

---
+++ packages/Nicelizhi/OneBuy/src/Resources/lang/fr/app.php
@@ -0,0 +1,78 @@
+<?php
+return [
+    'product' => [
+        'payment' => [
+            'creditCard' => [
+                'title' => 'Kreditkarte',
+                'card_number' => 'Kartennummer',
+                'Expiry' => 'Ablauf',
+                'cvc' => 'Lebenslauf',
+            ],
+            'paypal' => [
+                'title' => 'PayPal',
+                'description' => 'Nachdem du auf "Mit PayPal bezahlen" geklickt hast, wirst du zu PayPal weitergeleitet, um deinen Einkauf sicher abzuschließen.'
+            ],
+            'klarna' => [
+                'title' => 'klarna',
+                'description' => 'Nachdem du “Jetzt bezahlen” geklickt hast, wirst du zu Klarna weitergeleitet, um deinen Einkauf sicher abzuschließen.'
+            ],
+        'complete_secure_purchase' => 'VOLLSTÄNDIG SICHERER KAUF',
+        ],
+        'step' => [
+            'Select Quantity' => 'Wählen Sie die Menge',
+            'Payment' => 'Zahlung',
+            'Shipping Information' => 'Versand',
+            'Place Order' => 'Bestellung aufgeben',
+        ],
+        "order" => [
+            'Express Checkout' => 'Express-Kaufabwicklung',
+            'Secure Checkout' => 'Sichere Kasse',
+            'RECOMMENDED DEAL' => 'EMPFOHLENES ANGEBOT',
+            'Shipping' => 'Versand',
+            'Or Pay With Credit Card' => 'Oder zahlen Sie mit Kreditkarte',
+            'Credit Card Information' => 'Kreditkarteninformation',
+            'Your Card Info is invaild' => 'Ihre Karteninformationen sind ungültig',
+            'GUARANTEED' => 'GARANTIERT',
+            'SAFE' => 'SICHER',
+            'CHECKOUT' => 'KASSE',
+            'Enter your contact information' => 'Geben Sie Ihre Kontaktinformationen ein',
+            'Street Address' => 'Straßenadresse',
+            'Phone Number' => 'Telefonnummer',
+            'Last Name' => 'Familienname, Nachname',
+            'First Name' => 'Vorname',
+            'Email' => 'Email',
+            'City' => 'Stadt',
+            'Country' => 'Land',
+            'State/Province' => 'Staat/Provinz',
+            'Zip/Postal Code' => 'Postleitzahl',
+            'Order Summary' => 'Bestellübersicht',
+            'EDIT' => 'BEARBEITEN',
+            'QTY' => 'MENGE',
+            'Refund policy' => 'Rückgaberecht',
+            'Privacy Policy' => 'Datenschutzrichtlinie',
+            'Total' => 'Gesamt',
+            'Coupon' => 'Coupon',
+            'Shipping Insurance' => 'Transportversicherung',
+            'Discount' => 'Rabatt',
+            'Subtotal' => 'Zwischensumme',
+            '30 DAY GUARANTEE' => '30 TAGE GARANTIE',
+            'What customers are saying about' => 'Worüber Kunden sagen',
+            'Frequently Asked Questions' => 'Häufig gestellte Fragen',
+            'JUST NOW' => 'SOEBEN',
+            'COMPLETE SECURE PURCHASE' => 'VOLLSTÄNDIG SICHERER KAUF',
+            'Item' => 'Artikel',
+            'SELECT YOUR' => 'WÄHLE DEINE',
+            'All transactions are secure and encrypted' => 'Alle Transaktionen sind sicher und verschlüsselt',
+        ],
+        'footer' => [
+            'Shipping Delivery' => 'Versand & Lieferung',
+            'Refund policy' => 'Rückgaberecht',
+            'About US' => 'Über uns',
+            'Privacy Policy' => 'Datenschutzrichtlinie',
+            'Contact Us' => 'Kontaktiere uns',
+        ]
+    ],
+    'order-success' => [
+        
+    ],
+];
