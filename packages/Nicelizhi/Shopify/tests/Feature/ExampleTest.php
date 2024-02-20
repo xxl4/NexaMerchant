12f206220b49741b92e08d64fd82d9f1b201fb5a
Steve <nice.lizhi@gmail.com>
2024-02-20 01:37:15 +0000

    fix test for the onebuy version 1 checkout page

--- packages/Nicelizhi/Shopify/tests/Feature/ExampleTest.php
+++ packages/Nicelizhi/Shopify/tests/Feature/ExampleTest.php
@@ -1,5 +1,10 @@
 <?php
 
+test('the onebuy v1', function () {
+    $response = $this->get('/onebuy/8395243356390');
+    $response->assertStatus(200);
+});
+
 test('the shop returns a successful response', function () {
     $response = $this->get('/');
 
