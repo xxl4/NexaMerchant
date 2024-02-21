8ae0e42ce5ff41b795e4472562559cb0cea17a1a
xxl4 <nice.lizhi@gmail.com>
2024-02-21 18:30:04 +0800

    fix success

--- packages/Nicelizhi/Checkout/src/Resources/views/product-order-success-checkoutv1.blade.php
+++ packages/Nicelizhi/Checkout/src/Resources/views/product-order-success-checkoutv1.blade.php
@@ -17,27 +17,29 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
+<!-- Facebook Pixel Code -->
+<script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '410784348009950');
+    fbq('track', 'PageView');
+  </script>
+  <noscript>
+    <img height="1" width="1" style="display:none" 
+         src="https://www.facebook.com/tr?id=410784348009950&ev=PageView&noscript=1"/>
+  </noscript>
+  <!-- End Facebook Pixel Code -->
 <title>
         Thank you &ndash;
     </title>
 <script>
         function addVoluumImg(data) {
-            console.log('addVoluumImg');
-            return false;
-            var payout = data.info.payout || (data.info.total + data.info.currency); 
-            var bigImg = document.createElement("img");
-            bigImg.src = "https://tick.colapaco-op.com/conversion.gif?payout="+ payout +"&txid="+data.info._id.$oid;
-            bigImg.width="1";
-            bigImg.height="1";
-            document.body.appendChild(bigImg);
-
-            var bigImg1 = document.createElement("img");
-            bigImg1.src = "https://tick.ponira.com/conversion.gif?payout="+ payout +"&txid="+data.info._id.$oid;
-            bigImg1.width="1";
-            bigImg1.height="1";
-            document.body.appendChild(bigImg1);
-            console.log('addVoluumImg end');
         }
 
         function postVoluumConversion(data) {
@@ -134,7 +136,7 @@
             }
         }
 
-        //fbq('track', 'Purchase');
+        fbq('track', 'Purchase');
     </script>
 
 <link href="https://shop.hatmeo.com/css/timber.scss.css" rel="stylesheet" type="text/css" media="all">
