3d41d277eef8e86e4e253b75591598f7d807f200
xxl4 <nice.lizhi@gmail.com>
2024-02-23 11:14:23 +0800

    fix add table index

--- packages/Nicelizhi/Shopify/src/Database/Migrations/2024_02_23_103054_update_shopify_orders_table.php
+++ packages/Nicelizhi/Shopify/src/Database/Migrations/2024_02_23_103054_update_shopify_orders_table.php
@@ -18,13 +18,12 @@ return new class extends Migration
         Schema::table('shopify_products', function (Blueprint $table) {
             $table->index('shopify_store_id');
             $table->index('product_id');
-            $table->index(['shopify_store_id','product_id']);
+            $table->index(['shopify_store_id', 'product_id']);
         });
         Schema::table('shopify_orders', function (Blueprint $table) {
-            $table->unique('shopify_store_id','shopify_order_id','order_id')->unique();
             $table->index("shopify_store_id");
-            $table->index("shopify_order_id");
-            $table->index("order_id");
+            $table->index(['shopify_store_id', 'order_id']);
+            $table->index(['shopify_store_id', 'shopify_order_id']);
         });
     }
 
