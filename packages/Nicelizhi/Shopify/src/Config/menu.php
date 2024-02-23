ff4097e13e70bed9bd2cc810bde8b1c76e0565e8
xxl4 <nice.lizhi@gmail.com>
2024-02-23 13:47:17 +0800

    fix add shopify admin

--- packages/Nicelizhi/Shopify/src/Config/menu.php
+++ packages/Nicelizhi/Shopify/src/Config/menu.php
@@ -5,8 +5,8 @@ return [
      * Dashboard.
      */
     [
-        'key'        => 'shopify',
-        'name'       => 'admin::shopify.products.index',
+        'key'        => 'shopify-index',
+        'name'       => 'shopify::products.index',
         'route'      => 'shopify.products.index',
         'sort'       => 10,
         'icon'       => 'icon-dashboard',
