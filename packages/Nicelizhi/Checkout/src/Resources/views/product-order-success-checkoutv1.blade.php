58557124a28a059e4901f8e6ed70955242e7e13d
Steve <nice.lizhi@gmail.com>
2024-02-26 08:16:23 +0000

    add fb id

--- packages/Nicelizhi/Checkout/src/Resources/views/product-order-success-checkoutv1.blade.php
+++ packages/Nicelizhi/Checkout/src/Resources/views/product-order-success-checkoutv1.blade.php
@@ -35,6 +35,20 @@
          src="https://www.facebook.com/tr?id=410784348009950&ev=PageView&noscript=1"/>
   </noscript>
   <!-- End Facebook Pixel Code -->
+    <!-- Facebook Pixel Code -->
+<script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '946596946391407');
+  fbq('track', 'PageView');
+  fbq('track', 'ViewContent');
+</script>
 <title>
         Thank you &ndash;
     </title>
