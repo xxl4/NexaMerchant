0ba55440bc401e01931c18bfb1acc0d8662a87cb
Steve <nice.lizhi@gmail.com>
2024-02-27 02:21:48 +0000

    fix

--- packages/Nicelizhi/OneBuy/src/Resources/views/product-detail.blade.php
+++ packages/Nicelizhi/OneBuy/src/Resources/views/product-detail.blade.php
@@ -711,9 +711,9 @@ I agree with the <a href="/onebuy/page/refund-policy?locale=<?php echo strtolowe
 <div class="summary-footer summary-footer-mb">
 <div class="agree-block">
 <input type="checkbox" checked>
-I agree with the <a href="/onebuy/page/refund-policy?locale={{ app()->getLocale() }}" class="ajax" >
+I agree with the <a href="/onebuy/page/refund-policy?locale=<?php echo strtolower($default_country);?>" class="ajax" >
 @lang('onebuy::app.product.order.Refund policy') </a>
-& <a href="/onebuy/page/privacy-policy?locale={{ app()->getLocale() }}" class="ajax" >
+& <a href="/onebuy/page/privacy-policy?locale=<?php echo strtolower($default_country);?>" class="ajax" >
  @lang('onebuy::app.product.order.Privacy Policy') </a>
 . </div>
 
