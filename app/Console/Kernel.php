cf4ee35e49c3fe963f1f0cb21d3bafbc53ee9ca1
Steve <nice.lizhi@gmail.com>
2024-02-29 03:39:52 +0000

    fix the order get

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,8 @@ class Kernel extends ConsoleKernel
         # $schedule->command('invoice:cron')->dailyAt('3:00');
         # $schedule->command('product:index --type=price')->dailyAt('23:59');
         $schedule->command('shopify:order:post')->everyFiveMinutes()->withoutOverlapping(); // post order to shopify
-        $schedule->command('shopify:order:get --force=true')->hourly()->withoutOverlapping()->timezone('Asia/Shanghai')->between('9:00', '19:00');; // shopify order sync
+        $schedule->command('shopify:order:get --force=true')->hourly()->withoutOverlapping()->timezone('Asia/Shanghai')->between('9:00', '19:00'); // shopify order sync
+        $schedule->command('shopify:order:get:shipped --force=true')->hourly()->withoutOverlapping()->timezone('Asia/Shanghai')->between('9:00', '19:00'); // shopify order sync
     }
 
     /**
