fd8ed810fc3acc45f9c295c366d7f8d81ec8f184
xxl4 <nice.lizhi@gmail.com>
2024-03-14 14:58:59 +0800

    #96 add base manage package

---
+++ packages/Nicelizhi/Manage/src/Providers/EventServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Nicelizhi\Manage\Providers;
+
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event handler mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [
+        'admin.password.update.after' => [
+            'Nicelizhi\Admin\Listeners\Admin@afterPasswordUpdated',
+        ],
+
+        'checkout.order.save.after' => [
+            'Nicelizhi\Admin\Listeners\Order@afterCreated',
+        ],
+
+        'sales.order.cancel.after' => [
+            'Nicelizhi\Admin\Listeners\Order@afterCanceled',
+        ],
+
+        'sales.invoice.save.after' => [
+            'Nicelizhi\Admin\Listeners\Invoice@afterCreated',
+        ],
+
+        'sales.shipment.save.after' => [
+            'Nicelizhi\Admin\Listeners\Shipment@afterCreated',
+        ],
+
+        'sales.refund.save.after' => [
+            'Nicelizhi\Admin\Listeners\Refund@afterCreated',
+        ],
+
+        'core.channel.update.after' => [
+            'Nicelizhi\Admin\Listeners\ChannelSettingsChange@checkForMaintenanceMode',
+        ],
+    ];
+}
