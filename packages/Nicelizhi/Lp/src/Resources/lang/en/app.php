6676f6dff01f789d1df6fd5bfbdd089bbf25e906
xxl4 <nice.lizhi@gmail.com>
2024-03-14 11:36:43 +0800

    #117 add lp admin function

---
+++ packages/Nicelizhi/Lp/src/Resources/lang/en/app.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'lp' => [
+        'index' => "Lp Home"
+    ],
+    'admin' => [
+        'index' => [
+            'title' => 'LP Home',
+            'create-btn' => 'Create LP',
+        ]
+    ]
+];
---
+++ packages/Nicelizhi/Lp/src/Routes/store-admin-routes.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Nicelizhi\Lp\Http\Controllers\AdminLpController;
+
+/**
+ * Lp routes.
+ */
+Route::group(['middleware' => ['admin'], 'prefix' => config('app.admin_url')], function () {
+    Route::controller(AdminLpController::class)->prefix('lp')->group(function () {
+        Route::get('/', 'index')->name('admin.lp.index');
+
+        Route::get('create', 'create')->name('admin.lp.create');
+
+        Route::post('create', 'store')->name('admin.lp.store');
+
+        Route::get('edit/{id}', 'edit')->name('admin.lp.edit');
+
+        Route::put('edit/{id}', 'update')->name('admin.lp.update');
+
+        Route::delete('edit/{id}', 'delete')->name('admin.lp.delete');
+
+        Route::post('mass-delete', 'massDelete')->name('admin.lp.mass_delete');
+    });
+});
