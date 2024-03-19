d856d147b00413fda73af72b28aebef3c0f87c89
xxl4 <nice.lizhi@gmail.com>
2024-03-19 16:57:25 +0800

    fix the layout

--- packages/Nicelizhi/Manage/src/Resources/views/components/layouts/index.blade.php
+++ packages/Nicelizhi/Manage/src/Resources/views/components/layouts/index.blade.php
@@ -111,10 +111,7 @@
         <!-- ChartJS -->
         <script src="/themes/manage/AdminLTE/plugins/chart.js/Chart.min.js"></script>
 
-        <!-- AdminLTE for demo purposes -->
-        <script src="/themes/manage/AdminLTE/dist/js/demo.js"></script>
-        <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-        <script src="/themes/manage/AdminLTE/dist/js/pages/dashboard2.js"></script>
+        
 
     </body>
 </html>
