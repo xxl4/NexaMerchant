6a41b09a0b07d6f637831e0a369e9114af7e24b5
xxl4 <nice.lizhi@gmail.com>
2024-03-21 16:40:46 +0800

    fix show order detail

--- packages/Nicelizhi/Manage/src/Resources/views/sales/address.blade.php
+++ packages/Nicelizhi/Manage/src/Resources/views/sales/address.blade.php
@@ -1,25 +1,15 @@
-<div class="flex flex-col">
-    <p class="text-gray-800 font-semibold leading-6 dark:text-white">
-        {{ $address->company_name ?? '' }}
-    </p>
-
-    <p class="text-gray-800 font-semibold leading-6 dark:text-white">
-        {{ $address->name }}
-    </p>
-    
-    <p class="text-gray-600 dark:text-gray-300 leading-6">
-        {{ $address->address1 }}<br>
-
-        @if ($address->address2)
+<address>
+    <strong>{{ $address->company_name ?? '' }}</strong><br>
+    {{ $address->name }} <br>
+    {{ $address->address1 }}<br>
+    @if ($address->address2)
             {{ $address->address2 }}<br>
-        @endif
+    @endif
+    {{ $address->city }}<br>
 
-        {{ $address->city }}<br>
+    {{ $address->state }}<br>
 
-        {{ $address->state }}<br>
+    {{ core()->country_name($address->country) }} @if ($address->postcode) ({{ $address->postcode }}) @endif<br>
 
-        {{ core()->country_name($address->country) }} @if ($address->postcode) ({{ $address->postcode }}) @endif<br>
-
-        {{ __('admin::app.sales.orders.view.contact') }} : {{ $address->phone }}
-    </p>
-</div>
+    {{ __('admin::app.sales.orders.view.contact') }} : {{ $address->phone }}
+</address>
