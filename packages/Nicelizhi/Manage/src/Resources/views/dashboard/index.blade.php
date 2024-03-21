8face1d7a30172f8eef916ebc7b9ad746c194446
xxl4 <nice.lizhi@gmail.com>
2024-03-21 08:58:14 +0800

    fix the dashboard

--- packages/Nicelizhi/Manage/src/Resources/views/dashboard/index.blade.php
+++ packages/Nicelizhi/Manage/src/Resources/views/dashboard/index.blade.php
@@ -128,4 +128,23 @@
 
       </div>
     </section>
+
+    <script src="/themes/manage/AdminLTE/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="/themes/manage/AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- DataTables  & Plugins -->
+<script src="/themes/manage/AdminLTE/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="/themes/manage/AdminLTE/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="/themes/manage/AdminLTE/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="/themes/manage/AdminLTE/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="/themes/manage/AdminLTE/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="/themes/manage/AdminLTE/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script src="/themes/manage/AdminLTE/plugins/jszip/jszip.min.js"></script>
+<script src="/themes/manage/AdminLTE/plugins/pdfmake/pdfmake.min.js"></script>
+<script src="/themes/manage/AdminLTE/plugins/pdfmake/vfs_fonts.js"></script>
+<script src="/themes/manage/AdminLTE/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="/themes/manage/AdminLTE/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="/themes/manage/AdminLTE/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+
 </x-admin::layouts>
