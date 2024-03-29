90750854561e547beac00bf8862779ca79d79594
Steve <nice.lizhi@gmail.com>
2024-03-29 07:01:13 +0000

    #131 fulfillments_create

--- packages/Nicelizhi/Shopify/src/Http/Controllers/WebhooksController.php
+++ packages/Nicelizhi/Shopify/src/Http/Controllers/WebhooksController.php
@@ -7,16 +7,25 @@ use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Arr;
 use Nicelizhi\Shopify\Models\ShopifyProduct;
+use Nicelizhi\Shopify\Models\ShopifyOrder;
 //use Nicelizhi\Shopify\Repositories\ProductRepository;
+use Webkul\Sales\Repositories\OrderRepository;
+use Webkul\Sales\Repositories\ShipmentRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
 class WebhooksController extends Controller
 {
 
-    public function __construct()
+    private $shopify_store_id = null;
+
+    public function __construct(
+        protected OrderRepository $orderRepository,
+        protected ShopifyOrder $ShopifyOrder,
+        protected ShipmentRepository $shipmentRepository,
+    )
     {
-        
+        $this->shopify_store_id = config('shopify.shopify_store_id');
     }
 
     public function v1(Request $request) {
@@ -99,6 +108,100 @@ class WebhooksController extends Controller
 
     public function fulfillments_create(Request $request) {
         Log::info("fulfillments_create ".json_encode($request->all()));
+        $req = $request->all();
+
+        $shopify_order_id = $req['order_id'];
+
+        $shopifyNewOrder = $this->ShopifyOrder->where([
+            'shopify_store_id' => $this->shopify_store_id,
+            'shopify_order_id' => $shopify_order_id
+        ])->select(['order_id'])->first();
+
+        if(is_null($shopifyNewOrder)) {
+            Log::error("fulfillments_create ".$shopify_order_id);
+            return false;
+        }
+
+
+        $order = $this->orderRepository->findOrFail($shopifyNewOrder->order_id);
+
+        if (!$order->canShip()) {
+            Log::error("fulfillments_create cannot ship ".$shopify_order_id);
+            
+            return false;
+        }
+
+        // search local order_id and check it can send shippment
+
+        $shipment = [];
+        $shipment['carrier_title'] = $req['tracking_company'];
+        $shipment['track_number'] = $req['tracking_number'];
+        $shipment['source'] = 1;
+
+        $products = $order->items;
+
+        //var_dump($products);
+
+        $line_items = [];
+
+        foreach($products as $key=>$product) {
+            $sku = $product['additional'];
+
+            //var_dump($sku);
+
+            $skuInfo = explode('-', $sku['product_sku']);
+            //var_dump($skuInfo);
+            if(!isset($skuInfo[1])) {
+                $this->error("have error");
+                return false;
+            }
+
+            $line_item = [];
+            $line_item['variant_id'] = $skuInfo[1];
+            $line_item ['quantity'] = $product['qty_ordered'];
+            //$line_item ['requires_shipping'] = true;
+            //$line_item['item_id'] = $sku['variant_id'];
+            $line_item['order_item_id'] = $product['id'];
+
+            $line_items[$skuInfo[1]] = $line_item;
+
+            //array_push($line_items, $line_item);
+        }
+
+        //var_dump($line_items);exit;
+
+        // 获取已经发货的数据内容
+        $shipment_items = [];
+        foreach($req['line_items'] as $key=>$line_item) {
+            //var_dump($line_item['variant_id'],$line_item['quantity'], $line_items[$line_item['variant_id']]);
+            if(isset($line_items[$line_item['variant_id']])) {
+                if($line_items[$line_item['variant_id']]['quantity']==$line_item['quantity']) {
+                    $shipment_item = [];
+                    $shipment_items[$line_items[$line_item['variant_id']]['order_item_id']][1] = $line_item['quantity'];
+                    //array_push($shipment_items, $shipment_item);
+                    //$shipment_items[] = $shipment_item;
+                }
+            }
+        }
+
+        //var_dump($shipment_items);exit;
+
+        $shipment['items'] = $shipment_items;
+        //var_dump($line_items, $shipment_items, $shipment);
+
+        $data = [];
+        //$data['shipment']
+        $data['shipment'] = $shipment;
+
+        //var_dump($data);exit;
+
+        $response = $this->shipmentRepository->create(array_merge($data, [
+            'order_id' => $shopifyNewOrder->order_id,
+        ]));
+        
+        
+        
+
     }
 
     public function fulfillments_update(Request $request) {
