cf6e5f48218eb64a027a57668be745445d5865d1
xxl4 <nice.lizhi@gmail.com>
2024-04-12 20:04:37 +0800

    fix the order id

--- packages/Webkul/Sales/src/Generators/OrderSequencer.php
+++ packages/Webkul/Sales/src/Generators/OrderSequencer.php
@@ -42,7 +42,12 @@ class OrderSequencer extends Sequencer
      */
     public function getLastId()
     {
+        
+        return time();
+        
         $lastOrder = Order::query()->orderBy('id', 'desc')->limit(1)->first();
+
+
 
         return $lastOrder ? $lastOrder->id : 0;
     }
