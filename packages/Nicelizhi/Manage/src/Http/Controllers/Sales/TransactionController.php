1a897c2f4b8279b74350606a06d71b125ac886ce
xxl4 <nice.lizhi@gmail.com>
2024-04-18 09:46:34 +0800

    fix order transaction

--- packages/Nicelizhi/Manage/src/Http/Controllers/Sales/TransactionController.php
+++ packages/Nicelizhi/Manage/src/Http/Controllers/Sales/TransactionController.php
@@ -9,6 +9,7 @@ use Webkul\Sales\Repositories\InvoiceRepository;
 use Webkul\Sales\Repositories\ShipmentRepository;
 use Webkul\Sales\Repositories\OrderTransactionRepository;
 use Nicelizhi\Manage\DataGrids\Sales\OrderTransactionsDataGrid;
+use Nicelizhi\Manage\Helpers\SSP;
 use Webkul\Payment\Facades\Payment;
 
 class TransactionController extends Controller
@@ -35,7 +36,35 @@ class TransactionController extends Controller
     public function index()
     {
         if (request()->ajax()) {
-            return app(OrderTransactionsDataGrid::class)->toJson();
+            //return app(OrderTransactionsDataGrid::class)->toJson();
+            $table_pre = config("database.connections.mysql.prefix");
+            $table = $table_pre.'order_transactions';
+
+            // Table's primary key
+            $primaryKey = 'id';
+            
+            $columns = array(
+                //array( 'db' => 'id', 'dt' => 0 ),
+                array( 'db' => '`t`.`id`',  'dt' => 'id', 'field'=>'id','formatter' => function($d, $row){
+                    return '#'.$d;
+                } ),
+                array( 'db' => '`t`.`order_id`',  'dt' => 'order_id', 'field'=>'order_id'),
+                array( 'db' => '`t`.`type`',   'dt' => 'type', 'field'=>'type' ),
+                array( 'db' => '`t`.`amount`',   'dt' => 'amount', 'field'=>'amount' ),
+                array( 'db' => '`t`.`payment_method`',   'dt' => 'payment_method', 'field'=>'payment_method' ),
+                array( 'db' => '`t`.`invoice_id`',   'dt' => 'invoice_id', 'field'=>'invoice_id' ),
+                array( 'db' => '`t`.`transaction_id`',   'dt' => 'transaction_id', 'field'=>'transaction_id' ),
+                array( 'db' => '`t`.`created_at`',   'dt' => 'created_at', 'field'=>'created_at' ),
+            );
+            // SQL server connection information
+            $sql_details = [];
+
+            $joinQuery = "FROM `{$table}` AS `t` ";
+            $extraCondition = "";
+            //$extraCondition = "`a`.`address_type`='cart_shipping'";
+
+
+            return json_encode(SSP::simple( request()->input(), $sql_details, $table, $primaryKey, $columns, $joinQuery, $extraCondition ));
         }
 
         return view('admin::sales.transactions.index');
