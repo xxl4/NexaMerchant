eba4cb8c4956ac7a22c6a5176824f4f6e1976e26
xxl4 <nice.lizhi@gmail.com>
2024-04-22 15:37:11 +0800

    fix webhook order update

--- packages/Nicelizhi/Shopify/src/Http/Controllers/WebhooksController.php
+++ packages/Nicelizhi/Shopify/src/Http/Controllers/WebhooksController.php
@@ -58,6 +58,115 @@ class WebhooksController extends Controller
      */
     public function orders_updated(Request $request) {
         Log::info("orders_updated ".json_encode($request->all()));
+
+        $req = $request->all();
+
+        $shopify_order_id = $req['id'];
+
+        $shopifyNewOrder = $this->ShopifyOrder->where([
+            'shopify_store_id' => $this->shopify_store_id,
+            'shopify_order_id' => $shopify_order_id
+        ])->select(['order_id'])->first();
+
+        if(is_null($shopifyNewOrder)) {
+            Log::error("orders_updated ".$shopify_order_id);
+            return false;
+        }
+
+        $item = $req;
+
+        $shopifyNewOrder->admin_graphql_api_id = $item['admin_graphql_api_id'];
+        $shopifyNewOrder->app_id = $item['app_id'];
+        $shopifyNewOrder->browser_ip = $item['browser_ip'];
+        $shopifyNewOrder->buyer_accepts_marketing = $item['buyer_accepts_marketing'];
+        $shopifyNewOrder->cancel_reason = $item['cancel_reason'];
+        $shopifyNewOrder->cancelled_at = $item['cancelled_at'];
+        $shopifyNewOrder->cart_token = $item['cart_token'];
+        $shopifyNewOrder->checkout_id = $item['checkout_id'];
+        $shopifyNewOrder->checkout_token = $item['checkout_token'];
+        $shopifyNewOrder->client_details = $item['client_details'];
+        $shopifyNewOrder->closed_at = $item['closed_at'];
+        $shopifyNewOrder->company = @$item['company'];
+        $shopifyNewOrder->confirmation_number = $item['confirmation_number'];
+        $shopifyNewOrder->confirmed = $item['confirmed'];
+        $shopifyNewOrder->contact_email = $item['contact_email'];
+        $shopifyNewOrder->currency = $item['currency'];
+        $shopifyNewOrder->current_subtotal_price = $item['current_subtotal_price'];
+        $shopifyNewOrder->current_subtotal_price_set = $item['current_subtotal_price_set'];
+        $shopifyNewOrder->current_total_additional_fees_set = $item['current_total_additional_fees_set'];
+        $shopifyNewOrder->current_total_discounts = $item['current_total_discounts'];
+        $shopifyNewOrder->current_total_discounts_set = $item['current_total_discounts_set'];
+        $shopifyNewOrder->current_total_duties_set = $item['current_total_duties_set'];
+        $shopifyNewOrder->current_total_price = $item['current_total_price'];
+        $shopifyNewOrder->current_total_price_set = $item['current_total_price_set'];
+        $shopifyNewOrder->current_total_tax = $item['current_total_tax'];
+        $shopifyNewOrder->current_total_tax_set = $item['current_total_tax_set'];
+        $shopifyNewOrder->customer_locale = $item['customer_locale'];
+        $shopifyNewOrder->device_id = $item['device_id'];
+        $shopifyNewOrder->discount_codes = $item['discount_codes'];
+        $shopifyNewOrder->email = $item['email'];
+        $shopifyNewOrder->estimated_taxes = $item['estimated_taxes'];
+        $shopifyNewOrder->financial_status = $item['financial_status'];
+        $shopifyNewOrder->fulfillment_status = $item['fulfillment_status'];
+        $shopifyNewOrder->landing_site = $item['landing_site'];
+        $shopifyNewOrder->landing_site_ref = $item['landing_site_ref'];
+        $shopifyNewOrder->location_id = $item['location_id'];
+        $shopifyNewOrder->merchant_of_record_app_id = $item['merchant_of_record_app_id'];
+        $shopifyNewOrder->name = $item['name'];
+        $shopifyNewOrder->note = $item['note'];
+        $shopifyNewOrder->note_attributes = $item['note_attributes'];
+        $shopifyNewOrder->number = $item['number'];
+        $shopifyNewOrder->order_number = $item['order_number'];
+        $shopifyNewOrder->order_status_url = $item['order_status_url'];
+        $shopifyNewOrder->original_total_additional_fees_set = $item['original_total_additional_fees_set'];
+        $shopifyNewOrder->original_total_duties_set = $item['original_total_duties_set'];
+        $shopifyNewOrder->payment_gateway_names = $item['payment_gateway_names'];
+        $shopifyNewOrder->phone = $item['phone'];
+        $shopifyNewOrder->po_number = $item['po_number'];
+        $shopifyNewOrder->presentment_currency = $item['presentment_currency'];
+        $shopifyNewOrder->processed_at = $item['processed_at'];
+        $shopifyNewOrder->reference = $item['reference'];
+        $shopifyNewOrder->referring_site = $item['referring_site'];
+        $shopifyNewOrder->source_identifier = $item['source_identifier'];
+        $shopifyNewOrder->source_name = $item['source_name'];
+        $shopifyNewOrder->source_url = $item['source_url'];
+        $shopifyNewOrder->subtotal_price = $item['subtotal_price'];
+        $shopifyNewOrder->subtotal_price_set = $item['subtotal_price_set'];
+        $shopifyNewOrder->tags = $item['tags'];
+        $shopifyNewOrder->tax_exempt = $item['tax_exempt'];
+        $shopifyNewOrder->tax_lines = $item['tax_lines'];
+        $shopifyNewOrder->taxes_included = $item['taxes_included'];
+        $shopifyNewOrder->test = $item['test'];
+        $shopifyNewOrder->token = $item['token'];
+        $shopifyNewOrder->total_discounts = $item['total_discounts'];
+        $shopifyNewOrder->total_discounts_set = $item['total_discounts_set'];
+        $shopifyNewOrder->total_line_items_price = $item['total_line_items_price'];
+        $shopifyNewOrder->total_line_items_price_set = $item['total_line_items_price_set'];
+        $shopifyNewOrder->total_outstanding = $item['total_outstanding'];
+        $shopifyNewOrder->total_price = $item['total_price'];
+        $shopifyNewOrder->total_price_set = $item['total_price_set'];
+        
+        $shopifyNewOrder->total_shipping_price_set = $item['total_shipping_price_set'];
+        $shopifyNewOrder->total_tax = $item['total_tax'];
+        $shopifyNewOrder->total_tax_set = $item['total_tax_set'];
+        $shopifyNewOrder->total_tip_received = $item['total_tip_received'];
+        $shopifyNewOrder->total_weight = $item['total_weight'];
+        $shopifyNewOrder->user_id = $item['user_id'];
+        $shopifyNewOrder->billing_address = $item['billing_address'];
+        $shopifyNewOrder->customer = $item['customer'];
+        $shopifyNewOrder->discount_applications = $item['discount_applications'];
+        $shopifyNewOrder->fulfillments = $item['fulfillments'];
+        $shopifyNewOrder->line_items = $item['line_items'];
+        $shopifyNewOrder->payment_terms = $item['payment_terms'];
+        $shopifyNewOrder->refunds = $item['refunds'];
+        $shopifyNewOrder->shipping_address = $item['shipping_address'];
+        $shopifyNewOrder->shipping_lines = $item['shipping_lines'];
+        
+
+        $shopifyNewOrder->save();
+
+
+
     }
 
     /**
