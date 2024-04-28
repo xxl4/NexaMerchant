d253f9fa032c92f0206de388e4d94a0af687e6ee
xxl4 <nice.lizhi@gmail.com>
2024-04-28 09:58:54 +0800

    drop the bad links

--- packages/Nicelizhi/Manage/src/Resources/views/components/layouts/sidebar/index.blade.php
+++ packages/Nicelizhi/Manage/src/Resources/views/components/layouts/sidebar/index.blade.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="#" class="brand-link">
       <img src="{{ bagisto_asset('images/logo.svg') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
