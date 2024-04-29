3cc59b14b8b06efd1d7ef3626d4423a633f97fb6
xxl4 <nice.lizhi@gmail.com>
2024-04-29 14:51:49 +0800

    product add clarity code

--- packages/Nicelizhi/Lp/src/Resources/views/Lp/index.blade.php
+++ packages/Nicelizhi/Lp/src/Resources/views/Lp/index.blade.php
@@ -1,6 +1,15 @@
 <?php 
 echo $html->html;
 ?>
+
+<script type="text/javascript">
+  (function(c,l,a,r,i,t,y){
+      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+  })(window, document, "clarity", "script", "kruepex7cm");
+</script>
+
 <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo $gtag; ?>"></script>
 <script>
