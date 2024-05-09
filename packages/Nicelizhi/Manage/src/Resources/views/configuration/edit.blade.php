248f1844f5a8412709fe18b3fdcd0330d12c079e
xxl4 <nice.lizhi@gmail.com>
2024-05-09 09:39:41 +0800

    #222

--- packages/Nicelizhi/Manage/src/Resources/views/configuration/edit.blade.php
+++ packages/Nicelizhi/Manage/src/Resources/views/configuration/edit.blade.php
@@ -18,10 +18,47 @@
         @endif
     </x-slot:title>
 
-    <section class="content">
+    <section class="content-header">
         <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>{{ $title }}</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">{{ $title }}</li>
+                    </ol>
+                </div>
+            </div>
         </div>
     </section>
+    <form enctype="multipart/form-data" action="">
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+
+                <div class="col-md-12">
+                
+                    <div class="card card-primary">
+                        <div class="card-header">
+                            <h3 class="card-title">{{ $title }}</h3>
+                        </div>
+
+                        <div class="card-body">
+
+                            
+
+
+                        </div>
+
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+    </form>
 
 
 
