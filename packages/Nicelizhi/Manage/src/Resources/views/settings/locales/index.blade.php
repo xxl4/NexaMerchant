0e524581d3ecd5242a7f77074a24fc86a0136909
xxl4 <nice.lizhi@gmail.com>
2024-05-14 14:29:24 +0800

    locales

--- packages/Nicelizhi/Manage/src/Resources/views/settings/locales/index.blade.php
+++ packages/Nicelizhi/Manage/src/Resources/views/settings/locales/index.blade.php
@@ -12,7 +12,7 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1>@lang('admin::app.sales.orders.index.title')</h1>
+                <h1>@lang('admin::app.settings.locales.index.title')</h1>
             </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
