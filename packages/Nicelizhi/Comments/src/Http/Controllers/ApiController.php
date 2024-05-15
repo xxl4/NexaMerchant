4c4857dde5de13b42ff5716d20599baf897f80ba
xxl4 <nice.lizhi@gmail.com>
2024-05-15 17:02:16 +0800

    #260 comments api

--- packages/Nicelizhi/Comments/src/Http/Controllers/ApiController.php
+++ packages/Nicelizhi/Comments/src/Http/Controllers/ApiController.php
@@ -2,7 +2,8 @@
 namespace Nicelizhi\Comments\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use Google\Rpc\Context\AttributeContext\Response;
+use Illuminate\Http\Response;
+use Illuminate\Http\Request;
 use Webkul\Product\Repositories\ProductRepository;
 
 class ApiController extends Controller {
@@ -17,7 +18,7 @@ class ApiController extends Controller {
     /**
      * @var ProductRepository
      */
-    public function comments($slug) {
+    public function CommentsListSlug($slug) {
 
         $product = $product = $this->productRepository->findBySlug($slug);
         if(is_null($product)) {
@@ -48,15 +49,42 @@ class ApiController extends Controller {
         ], 200);
     }
 
-    public function index() {
+    public function index(Request $request) {
+        $per_page = request()->get('per_page', 10);
+        $page = request()->get('page', 0);
+        $product_id = request()->get('product_id', null);
+
+        $product = $this->productRepository->findBySlug($product_id);
+        
+        if(is_null($product)) {
+            return response()->json([
+                'message' => 'Product not found'
+            ], 404);
+        }
+
+        $reviewHelper = app('Webkul\Product\Helpers\Review');
+
+        $reviews = $product->reviews->where('status', 'approved')->skip($page*$per_page)->take($per_page);
+
+        $reviews = $reviews->map(function($review) {
+            $review->customer = $review->customer;
+            $review->pictures = $review->images;
+            return $review;
+        });
+
+
+        $data = [];
+        $data['reviews'] = $reviews;
+        $data['ratingValue'] = $reviewHelper->getAverageRating($product);
+        $data['reviewCount'] = $reviewHelper->getTotalReviews($product);
+        $data['getReviewsWithRatings'] = $reviewHelper->getReviewsWithRatings($product);
+        $data['getPercentageRating'] = $reviewHelper->getPercentageRating($product);
+
         return response()->json([
+            'data' => $data,
+            'code' => 200,
             'message' => 'success'
         ], 200);
     }
 
-    public function commentsListID($id) {
-        return response()->json([
-            'message' => 'success'
-        ], 200);
-    }
 }
--- packages/Nicelizhi/Comments/src/Routes/api.php
+++ packages/Nicelizhi/Comments/src/Routes/api.php
@@ -5,10 +5,9 @@ use Nicelizhi\Comments\Http\Controllers\ApiController;
 // api
 Route::group(['middleware' => ['locale', 'theme', 'currency'], 'prefix' => 'api'], function () {
 
-    Route::controller(ApiController::class)->prefix('comments')->group(function () {
+    Route::controller(ApiController::class)->prefix('reviews')->group(function () {
 
-        Route::get("reviews/{id}", "CommentsListID")->name("api.comments.list.id");
-        Route::get("reviews/{slug}", "CommentsListSlug")->name("api.comments.list.slug");
-        Route::get("reviews", "index")->name("api.comments.list.slug");
+        Route::get("{slug}", "CommentsListSlug")->name("api.comments.list.slug");
+        Route::get("", "index")->name("api.comments.list.slug");
     });
 });
