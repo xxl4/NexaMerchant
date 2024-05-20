553fb00ce252567976648049639597fc459d60f8
Steve <nice.lizhi@gmail.com>
2024-05-20 17:24:26 +0800

    Develop (#280)
    * debug v2 comments
    
    * #260 comments v2 and faq v2
    
    * add icon
    
    * upgrade apps platform docs
    
    * #260 comments
    
    * #260 comments backgroud color
    
    * create apps packages and shopify api docs
    
    * update docs
    
    * onebuy product detail api
    
    * fix
    
    * fix
    
    * onebuy api document
    
    * update
    
    * Develop (#275) (#276)
    
    * debug v2 comments
    
    * #260 comments v2 and faq v2
    
    * add icon
    
    * upgrade apps platform docs
    
    * #260 comments
    
    * #260 comments backgroud color
    
    * create apps packages and shopify api docs
    
    * update docs
    
    * onebuy product detail api
    
    * fix
    
    * fix
    
    * onebuy api document
    
    * update
    
    * fix Verified lang
    
    * add type on the lps modules
    
    * add type in the lps
    
    * sync main to develop (#279)
    
    * Develop (#275)
    
    * debug v2 comments
    
    * #260 comments v2 and faq v2
    
    * add icon
    
    * upgrade apps platform docs
    
    * #260 comments
    
    * #260 comments backgroud color
    
    * create apps packages and shopify api docs
    
    * update docs
    
    * onebuy product detail api
    
    * fix
    
    * fix
    
    * onebuy api document
    
    * update
    
    * lp add more params on page
    
    * Develop (#277)
    
    * debug v2 comments
    
    * #260 comments v2 and faq v2
    
    * add icon
    
    * upgrade apps platform docs
    
    * #260 comments
    
    * #260 comments backgroud color
    
    * create apps packages and shopify api docs
    
    * update docs
    
    * onebuy product detail api
    
    * fix
    
    * fix
    
    * onebuy api document
    
    * update
    
    * Develop (#275) (#276)
    
    * debug v2 comments
    
    * #260 comments v2 and faq v2
    
    * add icon
    
    * upgrade apps platform docs
    
    * #260 comments
    
    * #260 comments backgroud color
    
    * create apps packages and shopify api docs
    
    * update docs
    
    * onebuy product detail api
    
    * fix
    
    * fix
    
    * onebuy api document
    
    * update
    
    * fix Verified lang
    
    * Update Handler.php
    
    * Develop (#278)
    
    * debug v2 comments
    
    * #260 comments v2 and faq v2
    
    * add icon
    
    * upgrade apps platform docs
    
    * #260 comments
    
    * #260 comments backgroud color
    
    * create apps packages and shopify api docs
    
    * update docs
    
    * onebuy product detail api
    
    * fix
    
    * fix
    
    * onebuy api document
    
    * update
    
    * Develop (#275) (#276)
    
    * debug v2 comments
    
    * #260 comments v2 and faq v2
    
    * add icon
    
    * upgrade apps platform docs
    
    * #260 comments
    
    * #260 comments backgroud color
    
    * create apps packages and shopify api docs
    
    * update docs
    
    * onebuy product detail api
    
    * fix
    
    * fix
    
    * onebuy api document
    
    * update
    
    * fix Verified lang
    
    * add type on the lps modules
    
    * add type in the lps
    
    * lp sort
    
    * fix the express time and sync need cost time
    
    * fix default lang

--- packages/Nicelizhi/Shopify/src/Resources/views/products/index.blade.php
+++ packages/Nicelizhi/Shopify/src/Resources/views/products/index.blade.php
@@ -106,7 +106,7 @@
           {
             data: "product_id",
             render: function(data, type, row, meta) {
-              return '<a href="./products/sync/'+data+'" title="product sync"><i class="fas fa-sync"></i></a> <a href="./products/images/'+data+'/onebuy" title="product images"><i class="fas fa-images"></i></a> <a href="./products/comments/'+data+'/onebuy" title="product comments"><i class="fas fa-comments"></i></a> <a href="./products/info/'+data+'/onebuy" title="product info"><i class="fas fa-info-circle"></i> </a> ';
+              return '<a href="./products/sync/'+data+'" title="product sync"><i class="fas fa-sync"></i></a> <a href="./products/images/'+data+'/onebuy" title="product images"><i class="fas fa-images"></i></a> <a href="./products/comments/'+data+'/onebuy?locale={{ app()->getLocale() }}" title="product comments"><i class="fas fa-comments"></i></a> <a href="./products/info/'+data+'/onebuy?locale={{ app()->getLocale() }}" title="product info"><i class="fas fa-info-circle"></i> </a> ';
             }
           }
         ],
