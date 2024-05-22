337e949f20e367cda42532c56b4fded6bd4bf275
Steve <nice.lizhi@gmail.com>
2024-05-22 15:17:40 +0800

    Develop (#282)
    * debug v2 comments
    
    * #260 comments v2 and faq v2
    
    * add icon
    
    * upgrade apps platform docs
    
    * #260 comments
    
    * #260 comments backgroud color
    
    * create apps packages and shopify api docs
    
    * update docs
    
    * onebuy product detail api
    
    * fix
    
    * fix
    
    * onebuy api document
    
    * update
    
    * Develop (#275) (#276)
    
    * debug v2 comments
    
    * #260 comments v2 and faq v2
    
    * add icon
    
    * upgrade apps platform docs
    
    * #260 comments
    
    * #260 comments backgroud color
    
    * create apps packages and shopify api docs
    
    * update docs
    
    * onebuy product detail api
    
    * fix
    
    * fix
    
    * onebuy api document
    
    * update
    
    * fix Verified lang
    
    * add type on the lps modules
    
    * add type in the lps
    
    * sync main to develop (#279)
    
    * Develop (#275)
    
    * debug v2 comments
    
    * #260 comments v2 and faq v2
    
    * add icon
    
    * upgrade apps platform docs
    
    * #260 comments
    
    * #260 comments backgroud color
    
    * create apps packages and shopify api docs
    
    * update docs
    
    * onebuy product detail api
    
    * fix
    
    * fix
    
    * onebuy api document
    
    * update
    
    * lp add more params on page
    
    * Develop (#277)
    
    * debug v2 comments
    
    * #260 comments v2 and faq v2
    
    * add icon
    
    * upgrade apps platform docs
    
    * #260 comments
    
    * #260 comments backgroud color
    
    * create apps packages and shopify api docs
    
    * update docs
    
    * onebuy product detail api
    
    * fix
    
    * fix
    
    * onebuy api document
    
    * update
    
    * Develop (#275) (#276)
    
    * debug v2 comments
    
    * #260 comments v2 and faq v2
    
    * add icon
    
    * upgrade apps platform docs
    
    * #260 comments
    
    * #260 comments backgroud color
    
    * create apps packages and shopify api docs
    
    * update docs
    
    * onebuy product detail api
    
    * fix
    
    * fix
    
    * onebuy api document
    
    * update
    
    * fix Verified lang
    
    * Update Handler.php
    
    * Develop (#278)
    
    * debug v2 comments
    
    * #260 comments v2 and faq v2
    
    * add icon
    
    * upgrade apps platform docs
    
    * #260 comments
    
    * #260 comments backgroud color
    
    * create apps packages and shopify api docs
    
    * update docs
    
    * onebuy product detail api
    
    * fix
    
    * fix
    
    * onebuy api document
    
    * update
    
    * Develop (#275) (#276)
    
    * debug v2 comments
    
    * #260 comments v2 and faq v2
    
    * add icon
    
    * upgrade apps platform docs
    
    * #260 comments
    
    * #260 comments backgroud color
    
    * create apps packages and shopify api docs
    
    * update docs
    
    * onebuy product detail api
    
    * fix
    
    * fix
    
    * onebuy api document
    
    * update
    
    * fix Verified lang
    
    * add type on the lps modules
    
    * add type in the lps
    
    * lp sort
    
    * fix the express time and sync need cost time
    
    * fix default lang
    
    * fix payment images
    
    * index message update
    
    * coupon
    
    * coupon_code add

--- packages/Nicelizhi/Manage/src/Resources/views/components/layouts/header/index.blade.php
+++ packages/Nicelizhi/Manage/src/Resources/views/components/layouts/header/index.blade.php
@@ -95,7 +95,7 @@
             <!-- Message End -->
           </a>
           <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
+          <a href="{{ route('admin.notification.index') }" class="dropdown-item dropdown-footer">@lang('admin::app.notifications.view-all')</a>
         </div>
       </li>
 
