b2a32e2454fd32dbcccc0682d17a2f0477cc353f
xxl4 <nice.lizhi@gmail.com>
2024-05-27 16:12:14 +0800

    fix add console for paypal

--- packages/Webkul/Paypal/src/Console/Commands/Webhooks/Get.php
+++ packages/Webkul/Paypal/src/Console/Commands/Webhooks/Get.php
@@ -5,7 +5,7 @@ namespace Webkul\Paypal\Console\Commands\Webhooks;
 use Illuminate\Console\Command;
 use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Log;
-use Nicelizhi\Airwallex\Sdk\Airwallex as AirwallexSdk;
+use Webkul\Paypal\Payment\SmartButton;
 
 class Get extends Command
 {
@@ -69,49 +69,8 @@ class Get extends Command
      */
     public function handle()
     {
-        $client = new Client();
-
-        $this->apiKey = core()->getConfigData('sales.payment_methods.airwallex.apikey');
-
-        $this->clientId = core()->getConfigData('sales.payment_methods.airwallex.clientId');
-        $this->clientEmail = core()->getConfigData('sales.payment_methods.airwallex.clientEmail');
-        $this->clientPassword = core()->getConfigData('sales.payment_methods.airwallex.clientPassword');
-        $this->accountId = core()->getConfigData('sales.payment_methods.airwallex.accountId');
-        $this->paDC = core()->getConfigData('sales.payment_methods.airwallex.paDC');
-        $this->accountDC = core()->getConfigData('sales.payment_methods.airwallex.accountDC');
-
-        $this->paymentConfig = [
-            'clientId' => $this->clientId,
-            'apiKey' => $this->apiKey,
-            'clientEmail' => $this->clientEmail,
-            'clientPassword' => $this->clientPassword,
-            'accountId' => $this->accountId,
-            'paDC' => $this->paDC,
-            'accountDC' => $this->accountDC,
-        ];
-
-        $sdk = new AirwallexSdk($this->paymentConfig, $this->productionMode);
-
-        $token = $sdk->Authentication();
-
-        $input = [];
-
-        $input['events'] = [
-            'subscription.created',
-            'subscription.updated',
-            'subscription.cancelled',
-            'invoice.sent',
-            'invoice.paid',
-            'invoice.payment_failed',
-            'invoice.payment_attempt_failed'
-        ];
-
-        $input['request_id'] = time();
-        $input['url'] = "https://shop.hatmeo.com/api/airwallex/webhook";
-        $input['version'] = time();
-
-        $sdk->createWebhook(json_encode($input));
-
+        
+        $smartButton = new SmartButton();
 
        
     }
