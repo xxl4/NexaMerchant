853eba663cc715bf2cd2e4fccc965e57c4e6350a
xxl4 <nice.lizhi@gmail.com>
2024-05-28 18:48:22 +0800

    add import data

--- packages/Nicelizhi/OneBuy/src/Http/Controllers/ProductController.php
+++ packages/Nicelizhi/OneBuy/src/Http/Controllers/ProductController.php
@@ -301,6 +301,16 @@ class ProductController extends Controller
             $comments->images;
             return $comments;
         });
+        //$comments = [];
+        if(empty($comments)) {
+            $comments = $redis->hgetall($this->cache_prefix_key."product_comments_".$product['id']);
+            foreach($comments as $key=>$comment) {
+                $comment = json_decode($comment);
+                $comment->comment = $comment->content;
+                $comments[$key] = $comment;
+            }
+            //var_dump($comments);
+        }
 
         $paypal_client_id = core()->getConfigData('sales.payment_methods.paypal_smart_button.client_id');
 
