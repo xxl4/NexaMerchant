c40ecdabcd766511331f59aa67d4fe04f6968785
Steve <nice.lizhi@gmail.com>
2024-05-30 10:16:20 +0000

    fix sync product id comments

--- packages/Nicelizhi/Shopify/src/Console/Commands/Product/GetV3.php
+++ packages/Nicelizhi/Shopify/src/Console/Commands/Product/GetV3.php
@@ -336,6 +336,7 @@ class GetV3 extends Command
             sleep(1);
         }
 
-        Artisan::queue("onebuy:import:products:comment:from:judge")->onConnection('redis')->onQueue('shopify-products'); // import the shopify comments
+        //Artisan::queue("onebuy:import:products:comment:from:judge")->onConnection('redis')->onQueue('shopify-products'); // import the shopify comments
+        Artisan::queue("onebuy:import:products:comment:from:judge",['--prod_id'=>$shopify_pro_id])->onConnection('redis')->onQueue('shopify-products'); // import the shopify comments
     }
 }
