ae15e07adaa6e574ae4966de9ebdb1a382fbd7a1
Steve <nice.lizhi@gmail.com>
2024-05-31 02:41:19 +0000

    fix paypal id

--- packages/Webkul/Paypal/src/Listeners/Transaction.php
+++ packages/Webkul/Paypal/src/Listeners/Transaction.php
@@ -40,7 +40,7 @@ class Transaction
 
                 if ($transactionDetails['statusCode'] == 200) {
                     $this->orderTransactionRepository->create([
-                        'transaction_id' => isset($transactionDetails['result']['purchase_units'][0]['payments']['captures'][0]['id']) ? $transactionDetails['result']['purchase_units'][0]['payments']['captures'][0]['id'] : $transactionDetails['result']['id'],
+                        'transaction_id' => $transactionDetails['result']['id'],
                         'status'         => $transactionDetails['result']['status'],
                         'type'           => $transactionDetails['result']['intent'],
                         'amount'         => $transactionDetails['result']['purchase_units'][0]['amount']['value'],
