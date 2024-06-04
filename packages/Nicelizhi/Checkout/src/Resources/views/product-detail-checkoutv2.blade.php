08e9115d98a8bbc898859443b3c6cd4ba385cfb5
断线的纸鸢 <2544549156@qq.com>
2024-06-04 18:19:30 +0800

    fix:  style

--- packages/Nicelizhi/Checkout/src/Resources/views/product-detail-checkoutv2.blade.php
+++ packages/Nicelizhi/Checkout/src/Resources/views/product-detail-checkoutv2.blade.php
@@ -2943,7 +2943,7 @@
               $('.appalpay-button').show()
             }
           }
-          if (Object.keys(data.ads.size).lenght == 0) {
+          if (!data.ads.size.img) {
             $('#size-chart').hide()
           } else {
             $('#size-chart').show()
@@ -4507,24 +4507,24 @@
   <!-- <script type="text/javascript" src="js/checkout.js"></script> -->
 
   <script>
-    var spd = 100
-    var spdVal = 10
-    var cntDown = 5 * 60 * spdVal
-    setInterval(function() {
-      var mn, sc, ms
-      cntDown--
-      if (cntDown < 0) {
-        return false
-      }
-      mn = Math.floor(cntDown / spdVal / 60)
-      mn = mn < 10 ? '0' + mn : mn
-      sc = Math.floor((cntDown / spdVal) % 60)
-      sc = sc < 10 ? '0' + sc : sc
-      ms = Math.floor(cntDown % spdVal)
-      ms = ms < 10 ? '0' + ms : ms
-      var result = mn + ':' + sc
-      document.getElementById('stopwatch').innerHTML = result
-    }, spd)
+    // var spd = 100
+    // var spdVal = 10
+    // var cntDown = 5 * 60 * spdVal
+    // setInterval(function() {
+    //   var mn, sc, ms
+    //   cntDown--
+    //   if (cntDown < 0) {
+    //     return false
+    //   }
+    //   mn = Math.floor(cntDown / spdVal / 60)
+    //   mn = mn < 10 ? '0' + mn : mn
+    //   sc = Math.floor((cntDown / spdVal) % 60)
+    //   sc = sc < 10 ? '0' + sc : sc
+    //   ms = Math.floor(cntDown % spdVal)
+    //   ms = ms < 10 ? '0' + ms : ms
+    //   var result = mn + ':' + sc
+    //   document.getElementById('stopwatch').innerHTML = result
+    // }, spd)
     $(function() {
       var width = $(window).innerWidth()
       if (width > 767) {
