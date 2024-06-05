f670f8c34d73a60ef20084e6a169102977b8c6b9
Steve <nice.lizhi@gmail.com>
2024-06-05 01:05:00 +0000

    fix the google pay

--- packages/Nicelizhi/Checkout/src/Resources/views/product-detail-checkoutv2.blade.php
+++ packages/Nicelizhi/Checkout/src/Resources/views/product-detail-checkoutv2.blade.php
@@ -4080,19 +4080,32 @@
         .then(function(res) {
           console.log(res, 'googleres==');
           if (res.result === 200) {
+
+            Airwallex.init({
+              env: 'prod', // Setup which Airwallex env('staging' | 'demo' | 'prod') to integrate with
+              origin: window.location.origin, // Setup your event target to receive the browser events message
+            });
+
             const googlePayElement = Airwallex.createElement('googlePayButton', {
-              intent: {
-                // Required, googlePayButton uses intent_id and client_secret to prepare checkout
-                id: res.payment_intent_id,
-                client_secret: res.client_secret,
-              },
+              // intent: {
+              //   // Required, googlePayButton uses intent_id and client_secret to prepare checkout
+              //   id: res.payment_intent_id,
+              //   client_secret: res.client_secret,
+              // },
+              intent_id: res.payment_intent_id,
+              client_secret: res.client_secret,
               amount: {
                 value: res.order.base_grand_total,
                 currency: res.currency,
               },
+              emailRequired: true,
+              // billingAddressParameters: {
+              //   format: 'FULL',
+              //   phoneNumberRequired: true
+              // },
+              // billingAddressRequired: true,
               // countryCode: res.country,
               countryCode: 'HK',
-              buttonType: 'buy',
               origin: window.location.origin,
               autoCapture: true,
               merchantInfo: {
@@ -4100,7 +4113,7 @@
               },
             });
             const domGooglePay = googlePayElement.mount('googlePayButton');
-            domGooglePay.addEventListener('onReady', (event) => {
+            window.addEventListener('onReady', (event) => {
               $('#googlePayButton').removeClass('button-opacity')
               $('.pay-button').css('background', '#fff')
               /*
@@ -4109,7 +4122,8 @@
               // window.alert(event.detail);
               console.log(event.detail, event, 'googlePay ===  ready');
             });
-            domGooglePay.addEventListener('onSuccess', (event) => {
+            console.log("onSuccess");
+            window.addEventListener('onSuccess', (event) => {
               /*
                 ... Handle event on success
               */
@@ -4117,7 +4131,8 @@
               // console.log(event.detail);
               console.log(event.detail, event, 'googlePay ===  success');
             });
-            domGooglePay.addEventListener('onError', (event) => {
+            console.log("onError");
+            window.addEventListener('onError', (event) => {
               /*
                 ... Handle event on error
               */
