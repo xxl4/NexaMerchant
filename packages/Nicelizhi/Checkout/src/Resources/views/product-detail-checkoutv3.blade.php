2c7dcd54fbed641040b4658fe1d197f6260b521e
wangqian <wangqian@heomai.com>
2024-06-08 00:09:06 +0800

    fix: v2 v3

--- packages/Nicelizhi/Checkout/src/Resources/views/product-detail-checkoutv3.blade.php
+++ packages/Nicelizhi/Checkout/src/Resources/views/product-detail-checkoutv3.blade.php
@@ -3907,8 +3907,8 @@
       if (data.attr.attributes.length > 0) {
         $('#buy-select1').show()
       } else {
-        params.variant id = ''
-        params.product sku = data.sku
+        params.variant_id = ''
+        params.product_sku = data.sku
         $('#buy-select1').hide()
       }
       $('#product1').addClass('choose-p')
@@ -3943,8 +3943,8 @@
       if (data.attr.attributes.length > 0) {
         $('#buy-select2').show()
       } else {
-        params.variant id = ''
-        params.product sku = data.sku
+        params.variant_id = ''
+        params.product_sku = data.sku
         $('#buy-select2').hide()
       }
       $('#product2').addClass('choose-p')
@@ -3977,8 +3977,8 @@
       if (data.attr.attributes.length > 0) {
         $('#buy-select3').show()
       } else {
-        params.variant id = ''
-        params.product sku = data.sku
+        params.variant_id = ''
+        params.product_sku = data.sku
         $('#buy-select3').hide()
       }
 
@@ -4013,8 +4013,8 @@
       if (data.attr.attributes.length > 0) {
         $('#buy-select4').show()
       } else {
-        params.variant id = ''
-        params.product sku = data.sku
+        params.variant_id = ''
+        params.product_sku = data.sku
         $('#buy-select4').hide()
       }
       $('#product4').addClass('choose-p')
