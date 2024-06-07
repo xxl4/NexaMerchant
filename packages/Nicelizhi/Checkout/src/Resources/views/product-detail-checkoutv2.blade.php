7d3d980a8ff6d39ee7cc80745ce448e2abc873fe
Steve <nice.lizhi@gmail.com>
2024-06-07 11:17:12 +0000

    fix

--- packages/Nicelizhi/Checkout/src/Resources/views/product-detail-checkoutv2.blade.php
+++ packages/Nicelizhi/Checkout/src/Resources/views/product-detail-checkoutv2.blade.php
@@ -2672,8 +2672,8 @@
       getSkuListInfo();
       $('#pc-banner').attr('src', data.ads.pc.img);
       $('#mobile-banner').attr('src', data.ads.mobile.img);
-      $('.prod-name').text('<?php echo $data->product->name; ?>')
-      $('title').html('<?php echo $data->product->name; ?>')
+      $('.prod-name').text("<?php echo $data->product->name; ?>")
+      $('title').html("<?php echo $data->product->name; ?>")
       $('#buy-select1, #buy-select3, #buy-select4').hide()
       $('#footer-top-text').append(data.brand)
       var isPc = IsPC()
@@ -3850,6 +3850,7 @@
             });
             const domGooglePay = googlePayElement.mount('googlePayButton');
             domGooglePay.addEventListener('onReady', (event) => {
+              $('#loading').hide();
               $('#googlePayButton').removeClass('button-opacity')
               $('.pay-button').css('background', '#fff')
               /*
