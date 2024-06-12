c170050040db846c778cd7627a5ca050062ac3dc
xxl4 <nice.lizhi@gmail.com>
2024-06-12 17:02:03 +0800

    when goto url is empty and continue

--- packages/Nicelizhi/Lp/tests/Feature/CheckoutTest.php
+++ packages/Nicelizhi/Lp/tests/Feature/CheckoutTest.php
@@ -3,6 +3,7 @@ use Nicelizhi\Lp\Models\Lp;
 it('can check all the checkout page', function () {
     $items = Lp::where("status", 1)->select(['goto_url'])->get();
     foreach ($items as $item) {
+        if(empty($item->goto_url)) continue;
         echo $item->goto_url."\r\n";
         $response = $this->get($item->goto_url);
         var_dump($response->status());
