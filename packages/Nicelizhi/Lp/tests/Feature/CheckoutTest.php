0de79e4fd6bb09c398cd7c78347e3404117e3f7e
xxl4 <nice.lizhi@gmail.com>
2024-06-12 16:33:53 +0800

    fix add checkout test example

---
+++ packages/Nicelizhi/Lp/tests/Feature/CheckoutTest.php
@@ -0,0 +1,13 @@
+<?php
+use Nicelizhi\Lp\Models\Lp;
+it('can check all the checkout page', function () {
+    $items = Lp::where("status", 1)->select(['goto_url'])->get();
+    foreach ($items as $item) {
+        echo $item->goto_url."\r\n";
+        $response = $this->get($item->goto_url);
+        var_dump($response->status());
+        //var_dump($response);
+        //$response->assertStatus(200);
+        sleep(1);
+    }
+});
