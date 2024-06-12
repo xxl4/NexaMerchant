14254bfd06f27078bacb21afb55bc3396e26767f
xxl4 <nice.lizhi@gmail.com>
2024-06-12 17:40:36 +0800

    fix

--- packages/Nicelizhi/Lp/tests/Feature/CheckoutTest.php
+++ packages/Nicelizhi/Lp/tests/Feature/CheckoutTest.php
@@ -1,8 +1,9 @@
 <?php
 use Nicelizhi\Lp\Models\Lp;
 it('can check all the checkout page', function () {
-    $items = Lp::where("status", 1)->select(['goto_url'])->get();
+    $items = Lp::where("status", 1)->get();
     foreach ($items as $item) {
+        if(isset($item->goto_url)) continue;
         if(empty($item->goto_url)) continue;
         echo $item->goto_url."\r\n";
         $response = $this->get($item->goto_url);
