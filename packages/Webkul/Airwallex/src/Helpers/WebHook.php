71c3a55dacea278c780a2cef784ab51dbc12928a
xxl4 <nice.lizhi@gmail.com>
2024-06-13 13:04:13 +0800

    fix add webhook

--- packages/Webkul/Airwallex/src/Helpers/WebHook.php
+++ packages/Webkul/Airwallex/src/Helpers/WebHook.php
@@ -33,7 +33,7 @@ class WebHook {
     public function payment_dispute_challenged() {
 
         // check this data from the db
-        $dispute = \Webkul\Sales\Models\OrderDispute::where('dispute_id', $this->data['id'])->first();
+        $dispute = \Webkul\Sales\Models\OrderDispute::where('dispute_id', $this->data['data']['object']['id'])->first();
         if(is_null($dispute)) $dispute = $dispute = new \Webkul\Sales\Models\OrderDispute();
 
         //var_dump($this->data['data']['object']);exit;
@@ -53,6 +53,10 @@ class WebHook {
         $dispute->offer = $offer;
         $dispute->messages = $this->data['data']['object']['challenge_details'];
         $dispute->json = json_encode($this->data);
+
+        $merchant_order_id = str_replace("orderid_","", $this->data['data']['object']['merchant_order_id']);
+
+        $dispute->order_id = $merchant_order_id;
         $dispute->save();
 
     }
@@ -62,6 +66,23 @@ class WebHook {
     }
 
     public function payment_dispute_requires_response() {
+        $dispute = \Webkul\Sales\Models\OrderDispute::where('dispute_id', $this->data['data']['object']['id'])->first();
+        if(is_null($dispute)) $dispute = $dispute = new \Webkul\Sales\Models\OrderDispute();
+        $dispute->platform= "airwallex_".$this->data['data']['object']['payment_method_type'];
+        $dispute->dispute_id = $this->data['data']['object']['id'];
+
+        $dispute->status = $this->data['data']['object']['status'];
+        $refund_details = []; 
+        $refund_details['amount'] = $this->data['data']['object']['amount'];
+        $refund_details['currency'] = $this->data['data']['object']['currency'];
+        $refund_details['due_at'] = $this->data['data']['object']['due_at'];
+        $dispute->refund_details = $refund_details;
+
+        $merchant_order_id = str_replace("orderid_","", $this->data['data']['object']['merchant_order_id']);
+
+        $dispute->order_id = $merchant_order_id;
+        $dispute->json = json_encode($this->data);
+        $dispute->save();
 
     }
 
