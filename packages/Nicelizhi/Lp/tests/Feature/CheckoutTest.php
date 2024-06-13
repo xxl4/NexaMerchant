5e0990cd7624faae204d80d439c43d17e5b5e9c7
xxl4 <nice.lizhi@gmail.com>
2024-06-13 13:04:37 +0800

    fix add lp tests src

--- packages/Nicelizhi/Lp/tests/Feature/CheckoutTest.php
+++ packages/Nicelizhi/Lp/tests/Feature/CheckoutTest.php
@@ -1,11 +1,12 @@
 <?php
 use Nicelizhi\Lp\Models\Lp;
 it('can check all the checkout page', function () {
-    $items = Lp::where("status", 1)->get();
+    $items = Lp::where("status", 1)->orderBy("id", "desc")->limit(10)->get();
     foreach ($items as $item) {
+        echo $item->goto_url."\r\n";
         if(isset($item->goto_url)) continue;
         if(empty($item->goto_url)) continue;
-        echo $item->goto_url."\r\n";
+        
         $response = $this->get($item->goto_url);
         var_dump($response->status());
         //var_dump($response);
