b66976d533bb5f59782a2090684111411cf9834a
断线的纸鸢 <2544549156@qq.com>
2024-06-13 18:22:52 +0800

    fix: checkout v3

--- packages/Nicelizhi/Checkout/src/Resources/views/product-detail-checkoutv3.blade.php
+++ packages/Nicelizhi/Checkout/src/Resources/views/product-detail-checkoutv3.blade.php
@@ -1657,12 +1657,12 @@
       text-decoration: line-through;
     }
 
-    .mb20 {
-      margin-bottom: 20px;
+    .mb10 {
+      margin-bottom: 10px;
     }
 
-    .mt20 {
-      margin-top: 20px;
+    .mt10 {
+      margin-top: 10px;
     }
 
     .pr-price-single>p:nth-child(2) {
@@ -1802,19 +1802,9 @@
           <p class="text-Schritt-top">@lang('checkout::app.v2.You Can See By')</p>
           <img src="" alt="">
         </div>
-        <div class="three-description-box">
-        </div>
-        <div class="fl mt20">
-          <div class="order-msg mb20">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-fire" viewBox="0 0 16 16">
-              <path d="M8 16c3.314 0 6-2 6-5.5 0-1.5-.5-4-2.5-6 .25 1.5-1.25 2-1.25 2C11 4 9 .5 6 0c.357 2 .5 4-2 6-1.25 1-2 2.729-2 4.5C2 14 4.686 16 8 16Zm0-1c-1.657 0-3-1-3-2.75 0-.75.25-2 1.25-3C6.125 10 7 10.5 7 10.5c-.375-1.25.5-3.25 2-3.5-.179 1-.25 2 1 3 .625.5 1 1.364 1 2.25C11 14 9.657 15 8 15Z" />
-            </svg>
-            <strong id="order-msg-number" style="margin-left: 5px;"></strong>
-            <span class="ml5"> @lang('checkout::app.v3.sold in last') &nbsp;</span>
-            <strong id="order-msg-hour"></strong>
-            <span>&nbsp;@lang('checkout::app.v3.hours')</span>
-          </div>
-          <div class="pr-price-single mb20">
+        <div class="fl mt10">
+
+          <div class="pr-price-single m120">
             <p></p>
             <p></p>
             <p>
@@ -1824,28 +1814,40 @@
             </p>
           </div>
         </div>
+        <div class="three-description-box">
+        </div>
+
 
         <div class="btn-bx">
           <a href="#product2" class="comn-btn">@lang('checkout::app.v3.Buy Now')</a>
         </div>
-        <div class="fl">
-          <div class="shippingMsg mt20 mb20">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16">
-              <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z" />
-              <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z" />
+        <div class="fl mt10">
+          <div class="order-msg mb10">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-fire" viewBox="0 0 16 16">
+              <path d="M8 16c3.314 0 6-2 6-5.5 0-1.5-.5-4-2.5-6 .25 1.5-1.25 2-1.25 2C11 4 9 .5 6 0c.357 2 .5 4-2 6-1.25 1-2 2.729-2 4.5C2 14 4.686 16 8 16Zm0-1c-1.657 0-3-1-3-2.75 0-.75.25-2 1.25-3C6.125 10 7 10.5 7 10.5c-.375-1.25.5-3.25 2-3.5-.179 1-.25 2 1 3 .625.5 1 1.364 1 2.25C11 14 9.657 15 8 15Z" />
             </svg>
-            <span>@lang('checkout::app.v3.Estimated Delivery between')</span>
-            <strong id="shipping-date1"></strong>
-            <span>@lang('checkout::app.v3.and')</span>
-            <strong id="shipping-date2"></strong>
+            <strong id="order-msg-number" style="margin-left: 5px;"></strong>
+            <span class="ml5"> @lang('checkout::app.v3.sold in last') &nbsp;</span>
+            <strong id="order-msg-hour"></strong>
+            <span>&nbsp;@lang('checkout::app.v3.hours')</span>
           </div>
-          <div class="user-view mb20">
+          <div class="user-view mb10">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
               <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
               <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
             </svg>
             <strong class="user-view-num"></strong>
             <span>@lang('checkout::app.v3.People Looking For This Product')</span>
+          </div>
+          <div class="shippingMsg mt10 mb10">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16">
+              <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z" />
+              <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z" />
+            </svg>
+            <span>@lang('checkout::app.v3.Estimated Delivery between')</span>
+            <strong id="shipping-date1"></strong>
+            <span>-</span>
+            <strong id="shipping-date2"></strong>
           </div>
         </div>
 
