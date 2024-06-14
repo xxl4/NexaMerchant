06a0ee1a634ad021abdabcbdb0087c877c7f3ed8
xxl4 <nice.lizhi@gmail.com>
2024-06-14 09:50:22 +0800

    add payment_dispute_accepted for webhook

--- packages/Webkul/Airwallex/src/Helpers/WebHook.php
+++ packages/Webkul/Airwallex/src/Helpers/WebHook.php
@@ -70,12 +70,14 @@ class WebHook {
         if(is_null($dispute)) $dispute = $dispute = new \Webkul\Sales\Models\OrderDispute();
         $dispute->platform= "airwallex_".$this->data['data']['object']['payment_method_type'];
         $dispute->dispute_id = $this->data['data']['object']['id'];
+        $dispute->transaction_id =  $this->data['data']['object']['payment_intent_id'];
 
         $dispute->status = $this->data['data']['object']['status'];
         $refund_details = []; 
         $refund_details['amount'] = $this->data['data']['object']['amount'];
         $refund_details['currency'] = $this->data['data']['object']['currency'];
         $refund_details['due_at'] = $this->data['data']['object']['due_at'];
+       
         $dispute->refund_details = $refund_details;
 
         $merchant_order_id = str_replace("orderid_","", $this->data['data']['object']['merchant_order_id']);
@@ -90,8 +92,31 @@ class WebHook {
 
     }   
 
-    public function payment_dispute_accepted() {
 
+    public function payment_dispute_accepted() {
+        $dispute = \Webkul\Sales\Models\OrderDispute::where('dispute_id', $this->data['data']['object']['id'])->first();
+        if(is_null($dispute)) $dispute = $dispute = new \Webkul\Sales\Models\OrderDispute();
+
+        $dispute->platform= "airwallex_".$this->data['data']['object']['payment_method_type'];
+        $dispute->dispute_id = $this->data['data']['object']['id'];
+        $dispute->transaction_id =  $this->data['data']['object']['payment_intent_id'];
+
+        $dispute->status = $this->data['data']['object']['status'];
+        $refund_details = []; 
+        $refund_details['amount'] = $this->data['data']['object']['amount'];
+        $refund_details['currency'] = $this->data['data']['object']['currency'];
+        $refund_details['due_at'] = $this->data['data']['object']['due_at'];
+        $refund_details['issuer_comment'] = $this->data['data']['object']['issuer_comment'];
+        $refund_details['issuer_documents'] = $this->data['data']['object']['issuer_documents'];
+        $refund_details['accept_details'] = $this->data['data']['object']['accept_details'];
+        $dispute->refund_details = $refund_details;
+
+
+        $merchant_order_id = str_replace("orderid_","", $this->data['data']['object']['merchant_order_id']);
+
+        $dispute->order_id = $merchant_order_id;
+        $dispute->json = json_encode($this->data);
+        $dispute->save();
     }
 
 }
