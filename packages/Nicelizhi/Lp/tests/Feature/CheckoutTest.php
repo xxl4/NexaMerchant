63c2da3a21a1960d1efb06b3ed8f95687665de52
xxl4 <nice.lizhi@gmail.com>
2024-06-14 11:06:39 +0800

    fix lp test case

--- packages/Nicelizhi/Lp/tests/Feature/CheckoutTest.php
+++ packages/Nicelizhi/Lp/tests/Feature/CheckoutTest.php
@@ -4,7 +4,7 @@ it('can check all the checkout page', function () {
     $items = Lp::where("status", 1)->orderBy("id", "desc")->limit(10)->get();
     foreach ($items as $item) {
         echo $item->goto_url."\r\n";
-        if(isset($item->goto_url)) continue;
+        if(!isset($item->goto_url)) continue;
         if(empty($item->goto_url)) continue;
 
         $env = Config::get("app.env");
