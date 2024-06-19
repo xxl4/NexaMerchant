cf5e4e8cb3e8c89bff8d2b30aa28e9b678e33d00
xxl4 <nice.lizhi@gmail.com>
2024-06-19 15:49:21 +0800

    fix the apps remove

--- packages/Nicelizhi/Apps/src/Providers/AppsServiceProvider.php
+++ packages/Nicelizhi/Apps/src/Providers/AppsServiceProvider.php
@@ -54,6 +54,7 @@ class AppsServiceProvider extends ServiceProvider
             \Nicelizhi\Apps\Console\Commands\Install::class,
             \Nicelizhi\Apps\Console\Commands\UnInstall::class,
             \Nicelizhi\Apps\Console\Commands\Create::class,
+            \Nicelizhi\Apps\Console\Commands\Remove::class,
             \Nicelizhi\Apps\Console\Commands\Lists::class,
             \Nicelizhi\Apps\Console\Commands\Search::class,
         ]);
