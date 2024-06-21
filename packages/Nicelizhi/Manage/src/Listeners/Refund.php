c81cc41c5dfc918bff5edb384e42bd0d0ac27d93
xxl4 <nice.lizhi@gmail.com>
2024-06-21 16:54:28 +0800

    fix

--- packages/Nicelizhi/Manage/src/Listeners/Refund.php
+++ packages/Nicelizhi/Manage/src/Listeners/Refund.php
@@ -50,7 +50,6 @@ class Refund extends Base
             return;
         }
 
-        exit;
 
         if ($order->payment->method === 'paypal_smart_button') {
             /* getting smart button instance */
