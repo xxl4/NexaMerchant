7b5f38ef082e051acd2b7e64d15c4a9ff4b7c4d5
Steve <nice.lizhi@gmail.com>
2024-06-21 02:04:30 +0000

    fix refer

--- packages/Nicelizhi/Checkout/src/Http/Controllers/CheckoutV3Controller.php
+++ packages/Nicelizhi/Checkout/src/Http/Controllers/CheckoutV3Controller.php
@@ -85,6 +85,7 @@ class CheckoutV3Controller extends Controller{
 
         if(!empty($refer)) { 
             $request->session()->put('refer', $refer);
+            $request->session()->put('refer_'.$slug, $refer);
         }else{
             $refer = $request->session()->get('refer');
         }
