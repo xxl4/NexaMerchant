63b41fcd4d9f251e1a36cea45a326947074343fd
Steve <nice.lizhi@gmail.com>
2024-07-04 03:19:31 +0000

    fix lost on airwallex

--- packages/Webkul/Airwallex/src/Helpers/WebHook.php
+++ packages/Webkul/Airwallex/src/Helpers/WebHook.php
@@ -25,8 +25,78 @@ class WebHook {
         return $this;
     }
 
-    public function payment_dispute_lost() {
-        
+    public function payment_dispute_lost($orderRepository, $refundRepository) {
+        $dispute = \Webkul\Sales\Models\OrderDispute::where('dispute_id', $this->data['data']['object']['id'])->first();
+        if(is_null($dispute)) $dispute = $dispute = new \Webkul\Sales\Models\OrderDispute();
+
+        $dispute->platform= "airwallex_".$this->data['data']['object']['payment_method_type'];
+        $dispute->dispute_id = $this->data['data']['object']['id'];
+        $dispute->transaction_id =  $this->data['data']['object']['payment_intent_id'];
+
+        $dispute->status = $this->data['data']['object']['status'];
+        $refund_details = []; 
+        $refund_details['amount'] = $this->data['data']['object']['amount'];
+        $refund_details['currency'] = $this->data['data']['object']['currency'];
+        $refund_details['due_at'] = $this->data['data']['object']['due_at'];
+        $refund_details['issuer_comment'] = $this->data['data']['object']['issuer_comment'];
+        $refund_details['issuer_documents'] = $this->data['data']['object']['issuer_documents'];
+        $refund_details['accept_details'] = $this->data['data']['object']['accept_details'];
+        $dispute->refund_details = $refund_details;
+
+
+        $merchant_order_id = str_replace("orderid_","", $this->data['data']['object']['merchant_order_id']);
+
+        $dispute->order_id = $merchant_order_id;
+        $dispute->json = json_encode($this->data);
+        $dispute->save();
+
+        $order = $orderRepository->findOrFail($merchant_order_id);
+
+        if (! $order->canRefund()) {
+
+            return false;
+        }
+
+        $refud = [];
+        // 0: not refund money, 1: refund money
+
+
+       $refundData= [];
+
+       $params = [];
+
+       $order_items = $order->items;
+
+       //var_dump($order_items);
+
+
+
+       foreach ($order_items as $order_item) {
+           $refundData[$order_item->id] = $order_item->qty_ordered;
+       }
+
+       
+
+       $refud['refund']['items'] =  $refundData;
+
+       $totals = $refundRepository->getOrderItemsRefundSummary($refud['refund']['items'], $merchant_order_id);
+
+       //var_dump($totals);exit;
+
+       $refud['refund']['shipping'] = 9.99;
+       $refud['refund']['is_refund_money'] = 0;
+       $refud['refund']['adjustment_refund'] = 0;
+       $refud['refund']['adjustment_fee'] = 0;
+       $refud['refund']['custom_refund_amount'] = $this->data['data']['object']['amount'];
+
+       $refud['refund']['comment'] = $this->data['data']['object']['reason']['type'];
+
+       if(!empty($refud['refund']['custom_refund_amount'])) {
+           $refundAmount = $totals['grand_total']['price'] - $totals['shipping']['price'] + $refud['refund']['shipping'] + $refud['refund']['adjustment_refund'] - $refud['refund']['adjustment_fee'];
+           $refud['refund']['adjustment_fee'] = abs($refud['refund']['custom_refund_amount'] - $refundAmount);
+       }
+
+       $refundRepository->create(array_merge($refud, ['order_id' => $merchant_order_id]));
     }
 
     public function payment_dispute_won() {
