b2f68afd588ee0e03394ecc82a211674dcc57abd
Steve <nice.lizhi@gmail.com>
2024-07-11 05:37:52 +0000

    fix

--- packages/Nicelizhi/Shopify/src/Console/Commands/Order/Post.php
+++ packages/Nicelizhi/Shopify/src/Console/Commands/Order/Post.php
@@ -560,7 +560,7 @@ class Post extends Command
             $crm_channel = config('onebuy.crm_channel');
 
             
-            $url = "https://crm.heomai.com/api/offers/callBack?refer=".$cnv_id[1]."&revenue=".$order->grand_total."&currency_code=".$order->order_currency_code."&channel_id=".$crm_channel."&q_ty=".$q_ty."&email=".$shipping_address->email;
+            $url = "https://crm.heomai.com/api/offers/callBack?refer=".$cnv_id[1]."&revenue=".$order->grand_total."&currency_code=".$order->order_currency_code."&channel_id=".$crm_channel."&q_ty=".$q_ty."&email=".$item['email'];
             $res = $this->get_content($url);
             Log::info("post to bm 2 url ".$url." res ".json_encode($res));
 
