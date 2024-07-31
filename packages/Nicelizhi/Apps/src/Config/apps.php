beb06118917adaf39eb5d30bf889b93946923af2
xxl4 <nice.lizhi@gmail.com>
2024-07-31 18:11:10 +0800

    remove apps

--- packages/Nicelizhi/Apps/src/Config/apps.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'name' => 'Apps',
-    'base_dir' => env('APPS_BASE_DIR', 'apps'),
-    'url'   => env('APPS_URL', 'http://localhost'),
-    'token' => env('APPS_TOKEN') 
-];
--- packages/Nicelizhi/Apps/src/Config/system.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    [
-        'key'  => 'apps',
-        'name' => 'admin::app.configuration.index.general.title',
-        'info' => 'admin::app.configuration.index.general.info',
-        'sort' => 1,
-    ]
-];
