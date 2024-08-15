939e48f4f6d0272931552831aa8c390fc291e0d2
dagongren <wangqian@heomai>
2024-08-15 17:28:07 +0800

    add upsell

--- packages/Nicelizhi/OneBuy/src/Resources/views/checkout-success-v4.blade.php
+++ packages/Nicelizhi/OneBuy/src/Resources/views/checkout-success-v4.blade.php
@@ -1645,8 +1645,8 @@
               let title = $(this).data('title')
               $('.drawer-title').text(title)
               productList[0].description = title;
-              // let url = `/api/onebuy/product/detail/${id}?currency=EUR`
-              let url = `/api/onebuy/product/detail/8987102380314?currency=EUR`
+              let url = `/api/onebuy/product/detail/${id}?currency=EUR`
+              // let url = `/api/onebuy/product/detail/8987102380314?currency=EUR`
               let response = await fetch(url);
               if (!response.ok) {
                 throw new Error('Network response was not ok');
