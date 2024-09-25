aa57a30ebf2abae1dbd9076a80341586c7214f06
Steve <nice.lizhi@gmail.com>
2024-09-25 07:37:57 +0000

    fix airwallex

--- packages/Webkul/Airwallex/src/Controllers/AirwallexController.php
+++ packages/Webkul/Airwallex/src/Controllers/AirwallexController.php
@@ -165,7 +165,13 @@ class AirwallexController extends Controller
         } else if (isset($input['data']['object']['id'])) {
             // other webhook
 
-            Log::info("airwallex notification received for ".$input['name']." webhook id:" . $input['data']['object']['id']);
+            Log::info("airwallex notification received for  ".$input['name']." webhook id:" . $input['data']['object']['id']);
+
+            return response('OK', 200);
+        }else if(isset($input['data']['id'])) {
+            // other webhook
+
+            Log::info("airwallex notification received for data ".$input['name']." webhook id:" . $input['data']['id']);
 
             return response('OK', 200);
         }else {
