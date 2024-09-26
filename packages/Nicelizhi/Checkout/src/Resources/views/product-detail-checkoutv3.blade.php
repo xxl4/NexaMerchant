757bc8e06ce12ca274cc9c1c7964d15bde681fdf
Steve <nice.lizhi@gmail.com>
2024-09-26 10:43:35 +0000

    update

--- packages/Nicelizhi/Checkout/src/Resources/views/product-detail-checkoutv3.blade.php
+++ packages/Nicelizhi/Checkout/src/Resources/views/product-detail-checkoutv3.blade.php
@@ -93,25 +93,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
   </noscript>
 
-  <!-- Google Tag Manager -->
-  <script>
-    (function(w, d, s, l, i) {
-      w[l] = w[l] || [];
-      w[l].push({
-        'gtm.start': new Date().getTime(),
-        event: 'gtm.js'
-      });
-      var f = d.getElementsByTagName(s)[0],
-        j = d.createElement(s),
-        dl = l != 'dataLayer' ? '&l=' + l : '';
-      j.async = true;
-      j.src =
-        'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-      f.parentNode.insertBefore(j, f);
-    })(window, document, 'script', 'dataLayer', 'GTM-W6QBK3Q9');
-  </script>
-  <!-- End Google Tag Manager -->
-
   <style>
     img {
       /* aspect-ratio: attr(width)/attr(height); */
