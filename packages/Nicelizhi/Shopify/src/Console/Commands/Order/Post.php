501af96fc163342aef31017eb67bcf7933296ee9
Steve <nice.lizhi@gmail.com>
2024-09-29 07:34:29 +0000

    add order_id to crm system

--- packages/Nicelizhi/Shopify/src/Console/Commands/Order/Post.php
+++ packages/Nicelizhi/Shopify/src/Console/Commands/Order/Post.php
@@ -555,7 +555,7 @@ class Post extends Command
             $crm_channel = config('onebuy.crm_channel');
 
             
-            $url = "https://crm.heomai.com/api/offers/callBack?refer=".$cnv_id[1]."&revenue=".$order->grand_total."&currency_code=".$order->order_currency_code."&channel_id=".$crm_channel."&q_ty=".$q_ty."&email=".$item['email'];
+            $url = "https://crm.heomai.com/api/offers/callBack?refer=".$cnv_id[1]."&revenue=".$order->grand_total."&currency_code=".$order->order_currency_code."&channel_id=".$crm_channel."&q_ty=".$q_ty."&email=".$item['email']."&order_id=".$id;
             $res = $this->get_content($url);
             Log::info("post to bm 2 url ".$url." res ".json_encode($res));
 
